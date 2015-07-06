95581a05bcc0bcab6990da444cfa9d30ce51a220
Valentin Chemiere <valentin.chemiere@akretion.com>
2015-07-06 11:53:05 +0200

    Fix code review

---
+++ product.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  licence AGPL version 3 or later
+#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  Copyright (C) 2014 Akretion (http://www.akretion.com).
+#  @author Adrien CHAUSSENDE <adrien.chaussende@akretion.com>
+#
+##############################################################################
+
+from openerp import models, fields
+from openerp.osv import orm, fields as oldfields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    sale_prodlot_generation = fields.Boolean(
+        'Lot generation auto',
+        help="Forces to specifiy a Serial Number for all "
+             "lines containing this product since the confirm "
+             "of the Sale Order"
+    )
