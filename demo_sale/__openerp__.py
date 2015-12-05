b68d351e7516f60e4d2efdef99104ba958325008
David Beal <david.beal@akretion.com>
2015-12-05 13:01:09 +0100

    add module demo_sale

---
+++ demo_sale/__openerp__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# © 2015 David BEAL @ Akretion 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Demo Sales',
+    'summary': 'Sales demo mini',
+    'description': 'jeu de démo minimaliste pour faciliter le dev',
+    'version': '8.0.1.0.0',
+    'author': 'Akretion',
+    'maintainer': 'Akretion',
+    'category': 'sale',
+    'depends': [
+        'sale',
+    ],
+    'website': 'http://www.akretion.com/',
+    'data': [
+        'res.partner.csv',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
