34a4b7108c29f17f9f295c3ca38bb7d8094430c7
Sébastien BEAU <sebastien.beau@akretion.com>
2015-12-30 14:22:50 +0100

    [IMP] add the support of default value, Indeed a fake line of variant is injected in order to give the possibility to set the template value on this fake variant, this fake variant have for variant configuration "default"

--- product_variant_import_export/models/product.py
+++ product_variant_import_export/models/product.py
@@ -71,3 +71,62 @@ class ProductTemplate(models.Model):
                 record.with_context(from_template_id=record.id)
                 )._export_rows(fields)
         return data
+
+    @api.model
+    def _get_variant_id_from_configuration(
+            self, tmpl_id, variant_configuration):
+        product_obj = self.env['product.product']
+        domain = [('product_tmpl_id', '=', tmpl_id)]
+        value_ids = []
+        value_obj = self.env['product.attribute.value']
+        for option in variant_configuration.split('\n'):
+            attribute_name, value_name = option.split(':')
+            value = value_obj.search([
+                ('attribute_id', '=', attribute_name),
+                ('name', '=', value_name),
+                ])
+            domain.append(('attribute_value_ids', '=', value.id))
+        product = product_obj.search(domain)
+        return product.id
+
+    @api.model
+    def _update_product_ids_from_configuration(self, record):
+        if record.get('.id'):
+            tmpl_id = record['.id']
+        elif record.get('id'):
+            xml_id = record['id']
+            if not '.' in xml_id:
+               xml_id = u'.' + xml_id
+            __, __, tmpl_id = self.env['ir.model.data'].xmlid_lookup(xml_id)
+        else:
+            # If there is not template id then we can not look at the variant
+            return
+        for variant in record['product_variant_ids']:
+            variant_id = self._get_variant_id_from_configuration(
+                tmpl_id, variant['variant_configuration'])
+            if variant_id:
+                variant.pop('variant_configuration')
+                variant['.id'] = variant_id
+
+    @api.model
+    def _process_default_template_value(self, record):
+        default = None
+        for variant in list(record['product_variant_ids']):
+            if variant['variant_configuration'] == 'default':
+                variant.pop('variant_configuration')
+                record.update(variant)
+                record['product_variant_ids'].remove(variant)
+                break
+
+    @api.model
+    def _extract_records(self, fields_, data,
+                         context=None, log=lambda a: None):
+        for record, rows in super(ProductTemplate, self)._extract_records(
+                fields_, data, context=context, log=log):
+            if (['product_variant_ids', 'variant_configuration'] in fields_
+                    and (['id'] in fields_ or ['.id'] in fields_)
+                    and not ['product_variant_ids', 'id'] in fields_
+                    and not ['product_variant_ids', 'id'] in fields_):
+                self._process_default_template_value(record)
+                self._update_product_ids_from_configuration(record)
+            yield record, rows
