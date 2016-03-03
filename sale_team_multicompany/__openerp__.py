c89183ed60fde78091851da47e706b4d4732dac9
chafique-delli <chafique.delli@akretion.com>
2016-03-03 13:11:34 +0100

    replace double quotes by simple quote in __openerp__.py

--- sale_team_multicompany/__openerp__.py
+++ sale_team_multicompany/__openerp__.py
@@ -2,28 +2,28 @@
 # © <2016> <chafique.delli@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale Team Multicompany",
-    "summary": "Add companies in sales team and "
-    "filter the sales team from the company",
-    "version": "8.0.0.1.0",
-    "category": "Sales Management",
-    "website": "http://akretion.com",
-    "author": "Akretion",
-    "license": "AGPL-3",
-    "application": False,
-    "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
+    'name': 'Sale Team Multicompany',
+    'summary': 'Add companies in sales team and '
+    'filter the sales team from the company',
+    'version': '8.0.0.1.0',
+    'category': 'Sales Management',
+    'website': 'http://akretion.com',
+    'author': 'Akretion',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
     },
-    "depends": [
+    'depends': [
         'sales_team',
     ],
-    "data": [
+    'data': [
         'security/ir.rule.csv',
     ],
-    "demo": [
+    'demo': [
     ],
-    "qweb": [
+    'qweb': [
     ]
 }
