cfc625a9dc557df89b6311cd8e747196f4777277
chafique-delli <chafique.delli@akretion.com>
2016-03-03 16:13:04 +0100

    add sale_team_view.xml in __openerp.py__ and add french translate and add tests

--- sale_team_multicompany/__init__.py
+++ sale_team_multicompany/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © <2016> <chafique.delli@akretion.com>
+# © 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import sale_team
--- sale_team_multicompany/__openerp__.py
+++ sale_team_multicompany/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © <2016> <chafique.delli@akretion.com>
+# © 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Sale Team Multicompany',
@@ -20,6 +20,7 @@
         'sales_team',
     ],
     'data': [
+        'sale_team_view.xml',
         'security/ir.rule.csv',
     ],
     'demo': [
