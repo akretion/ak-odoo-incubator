84d348e0f2bda12913c7aec04d5dd62270c41d54
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-03-04 18:01:51 +0100

    [FIX] flake8;

--- global_discount_rule/price_rule.py
+++ global_discount_rule/price_rule.py
@@ -2,7 +2,7 @@
 # © 2015 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
+from openerp import fields, models
 
 
 class PriceDiscountBuilder(models.Model):
--- stock_warehouse_clean_delete/__openerp__.py
+++ stock_warehouse_clean_delete/__openerp__.py
@@ -6,23 +6,13 @@
 {
     'name': "Clean Warehouse delete",
     'description': """
-        Remote records related to a warehouse when you delete it (routes, sequences etc...)
-    """,
-
+Remote records related to a warehouse when you delete it
+(routes, sequences etc...)""",
     'author': "Akretion",
     'website': "http://www.akretion.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check <odoo>/addons/base/module/module_data.xml of the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['stock'],
-    'data': [],
-
-    'demo': [],
-
-    'tests': [
+    'depends': [
+        'stock',
     ],
 }
