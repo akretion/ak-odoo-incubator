e759ac5fb2e180e79aca59891c739da8372748c3
chafique-delli <chafique.delli@akretion.com>
2016-03-11 10:34:35 +0100

    replaced except_orm by new api

--- sale_team_multicompany/tests/test_sales_team_multicompany.py
+++ sale_team_multicompany/tests/test_sales_team_multicompany.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
-from openerp.osv.orm import except_orm as AccessError
+from openerp.exceptions import Warning as AccessError
 
 
 class TestSalesTeamMultiCompany(TransactionCase):
