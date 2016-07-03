9de1a348f63dbd420a8e8bf33ffefacbd4c20e47
David Beal <david.beal@akretion.com>
2016-07-03 11:39:40 +0200

    [ADD] module prevent2make_order

---
+++ prevent2make_order/__openerp__.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+# © 2015 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Prevent to Make Orders',
+    'summary': 'Avoid to create test data in sale, purchase',
+    'description': """
+        Forbide to create these objects: sale.order, purchase.order,
+        stock.picking, account.invoice
+        by raising an alert to the user on recording.
+        This module is only useful before to reach 'production' step of
+        ERP implementation project.
+        """,
+    'version': '8.0.1.0.0',
+    'author': 'Akretion',
+    'maintainer': 'Akretion',
+    'category': 'sale',
+    'depends': [
+        'sale_stock',
+        'purchase',
+        'account',
+    ],
+    'website': 'http://www.akretion.com/',
+    'data': [
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
