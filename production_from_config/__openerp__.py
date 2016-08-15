b7c285ac3e65746ab9216e31eefd5156e5e3de93
Florian da Costa <florian.dacosta@akretion.com>
2016-08-15 17:24:33 +0200

    Add production_from_config

---
+++ production_from_config/__openerp__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# © 2016 David BEAL @ Akretion <david.beal@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Production From Config',
+    'version': '9.0.0.1.1',
+    'category': 'Manufacture',
+    'description': """
+        Make consume line from lot configuration
+    """,
+    'author': 'Akretion',
+    'website': 'www.akretion.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_lot_config',
+        'sale_order_lot_mrp',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
