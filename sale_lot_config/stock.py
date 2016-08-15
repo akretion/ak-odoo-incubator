6c1610443b700786a5562c9294ad4f28f5c9f6c2
Florian da Costa <florian.dacosta@akretion.com>
2016-08-15 17:24:12 +0200

    Add sale_lot_config

---
+++ sale_lot_config/stock.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# © 2016 David BEAL @ Akretion <david.beal@akretion.com>
+#        Sébastien Beau <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.production.lot'
+
+    config = fields.Serialized(
+        'Configuration',
+        readonly=True,
+        help="Allow to set custom configuration")
