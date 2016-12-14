b33fe0eedcf48abf4138dba716e1f9188bd7d674
Raph <raph@poir.es>
2016-12-14 18:17:48 +0100

    Add sale_shop_keychain and delivery_roulier_accounts_on_sale_shop

---
+++ delivery_roulier_accounts_on_sales_shop/__openerp__.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# @author Raphael Reverdy <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Delivery accounts on Shop',
+    'version': '9.0.0.0.0',
+    'author': 'Akretion',
+    'summary': 'Set an account for delivery on shop',
+    'maintainer': 'Akretion, Odoo Community Association (OCA)',
+    'category': 'Warehouse',
+    'depends': [
+        'keychain',
+        'delivery_roulier',
+        'sale_shop',
+        'sale_shop_keychain',
+    ],
+    'website': 'http://www.akretion.com/',
+    'data': [
+    ],
+    'demo': [
+    ],
+    'external_dependencies': {
+    },
+    'tests': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    'application': False,
+}
