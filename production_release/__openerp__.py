eadc96169a4b923ce323d0ac66e2ebebbdce76fa
David Beal <david.beal@akretion.com>
2017-01-24 17:19:31 +0100

    ADD production_release module

---
+++ production_release/__openerp__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# © 2017 David BEAL @ Akretion <david.beal@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Production Release",
+    'description': """
+Record each new production stage
+    """,
+    'author': "Akretion",
+    'website': "http://www.akretion.com",
+    'category': 'Tools',
+    'version': '8.0.1.0.1',
+    'depends': [
+    ],
+    'data': [
+        'release_view.xml',
+        'security/ir.model.access.csv',
+    ],
+}
