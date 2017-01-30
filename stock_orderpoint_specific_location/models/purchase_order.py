86078eb8def1e6a03644f51d78755a617b2c789a
Florian da Costa <florian.dacosta@akretion.com>
2017-01-30 14:22:45 +0100

    Display the specific location field in purchase orders

--- stock_orderpoint_specific_location/models/purchase_order.py
+++ stock_orderpoint_specific_location/models/purchase_order.py
@@ -27,6 +27,9 @@ class PurchaseOrder(models.Model):
 
     specific_location_id = fields.Many2one(
         'stock.location', string='Specific Location')
+    default_location_dest_id = fields.Many2one(
+        related='picking_type_id.default_location_dest_id',
+        string='Default Location')
 
     @api.multi
     def _get_destination_location(self):
@@ -36,4 +39,6 @@ class PurchaseOrder(models.Model):
         else:
             return super(PurchaseOrder, self)._get_destination_location()
 
-
+    @api.onchange('picking_type_id')
+    def onchange_specific_location(self):
+        self.specific_location_id = False
