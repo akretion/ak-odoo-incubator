a8d99aaba78063149cb233a9ada3185f0c079fd6
beau sebastien <sebastien.beau@akretion.com>
2017-02-24 18:20:46 +0100

    [ADD] add module to edit in mass the account_move_line and update the invoice linked at the same time (#30)

---
+++ account_move_line_mass_edit_account/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account move line mass edit account",
+    "summary": "Give the possibility to edit in mass the account on move line",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "wizards/mass_edit_move_line_account_view.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
---
+++ account_move_line_mass_edit_account/wizards/mass_edit_move_line_account.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+from openerp.exceptions import Warning as UserError
+
+
+class MassEditMoveLineAccount(models.TransientModel):
+    _name = 'mass.edit.move.line.account'
+    _description = 'Mass Edit Account'
+
+    account_id = fields.Many2one(
+        'account.account',
+        'Account')
+
+    def _update_account_on_invoice(self, line):
+        self.ensure_one()
+        existing_lines = self.env['account.move.line'].search([
+            ('account_id', '=', line.account_id.id),
+            ('move_id', '=', line.move_id.id),
+            ])
+        if len(existing_lines) > 1:
+            # TODO we should be able to solve that case
+            # but it's harder because we do not have the link between
+            # the move line and the invoice line
+            # If we have only one line in the account
+            # we can update all invoice line with this account without error
+            raise UserError(
+                "Can not update the line as other line "
+                "are in the same account")
+        else:
+            invoice_lines = self.env['account.invoice.line'].search([
+                ('account_id', '=', line.account_id.id),
+                ('invoice_id', '=', line.invoice.id),
+                ])
+            invoice_lines.write({'account_id': self.account_id.id})
+
+    @api.multi
+    def change_account(self):
+        self.ensure_one()
+        move_lines = self.env['account.move.line'].browse(
+            self._context['active_ids'])
+        for line in move_lines:
+            if line.invoice:
+                line = line.with_context(from_parent_object=True)
+                self._update_account_on_invoice(line)
+            line.account_id = self.account_id
+        return True
