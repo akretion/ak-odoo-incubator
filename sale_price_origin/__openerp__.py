4b9342d50410d64acba151372ec91409ad3f1f63
David Beal <david.beal@akretion.com>
2017-03-08 19:19:12 +0100

    ADD sale_price_origin

---
+++ sale_price_origin/__openerp__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# © 2017 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Sale Price Origin',
+    'summary': 'Keep product pricelist item used on sale order line',
+    'version': '8.0.1.0.0',
+    'author': 'Akretion',
+    'maintainer': 'Akretion',
+    'category': 'Sale',
+    'depends': [
+        'sale',
+    ],
+    'website': 'http://www.akretion.com/',
+    'data': [
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
