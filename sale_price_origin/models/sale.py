1b536f7ca718cf8fd67becac8ef0f058aa01a1ab
David Beal <david.beal@akretion.com>
2017-03-09 17:08:22 +0100

    FIX sale_price_origin

--- sale_price_origin/models/sale.py
+++ sale_price_origin/models/sale.py
@@ -3,40 +3,29 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, fields, models
-# from openerp import SUPERUSER_ID
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    @api.multi
-    def get_pricelist_item(self):
-        for rec in self:
-            origins = {}
-            for line in rec.order_line:
-                if line.product_id:
-                    infos = rec.pricelist_id.price_rule_get(
-                        line.product_id.id,
-                        line.product_uom_qty, rec.partner_id.id)
-                    if isinstance(infos, dict):
-                        # infos i.e. {741568: {1: (239.4, 4430)}}
-                        price, item = infos[infos.keys()[0]]
-                        if round(line.price_unit, 2) == round(price, 2):
-                            origins[line.id] = {'price_origin': item}
-            o2m = [(1, k, v) for k, v in origins.items()]
-            rec.write({'order_line': o2m})
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     price_origin = fields.Many2one(
-        comodel_name='product.pricelist.item', ondelete='cascade',
+        comodel_name='product.pricelist.item', string="Price Origin",
+        compute='_compute_price_origin', readonly=True, ondelete='cascade',
         help="Define which product pricelist item has been used "
-             "to compute the price")
+             "to compute the price unit")
 
     @api.multi
-    def write(self, vals):
-        if 'price_origin' not in vals:
-            vals['price_origin'] = False
-        return super(SaleOrderLine, self).write(vals)
+    @api.depends('product_id', 'price_unit')
+    def _compute_price_origin(self):
+        """ According to price update, compute rule which brings the price
+        """
+        for line in self:
+            if line.product_id:
+                infos = line.order_id.pricelist_id.price_rule_get(
+                    line.product_id.id, line.product_uom_qty,
+                    line.order_id.partner_id.id)
+                if isinstance(infos, dict):
+                    # infos i.e. {1: (239.4, 4430)}
+                    price, item = infos[infos.keys()[0]]
+                    if round(line.price_unit, 2) == round(price, 2):
+                        line.price_origin = item
