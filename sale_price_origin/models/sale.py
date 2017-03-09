8829777cb260b298aecae2b004c8b908ed38790c
David Beal <david.beal@akretion.com>
2017-03-09 22:00:42 +0100

    FIX store pricelist_item_id in sale line

--- sale_price_origin/models/sale.py
+++ sale_price_origin/models/sale.py
@@ -8,15 +8,16 @@ from openerp import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    price_origin = fields.Many2one(
+    pricelist_item_id = fields.Many2one(
         comodel_name='product.pricelist.item', string="Price Origin",
-        compute='_compute_price_origin', readonly=True, ondelete='cascade',
+        compute='_compute_price_item', store=True,
+        readonly=True, ondelete='cascade',
         help="Define which product pricelist item has been used "
              "to compute the price unit")
 
     @api.multi
     @api.depends('product_id', 'price_unit')
-    def _compute_price_origin(self):
+    def _compute_price_item(self):
         """ According to price update, compute rule which brings the price
         """
         for line in self:
@@ -28,4 +29,4 @@ class SaleOrderLine(models.Model):
                     # infos i.e. {1: (239.4, 4430)}
                     price, item = infos[infos.keys()[0]]
                     if round(line.price_unit, 2) == round(price, 2):
-                        line.price_origin = item
+                        line.pricelist_item_id = item
