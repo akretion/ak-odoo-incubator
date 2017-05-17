4271623176f85f61acae323033eaa872149ecb27
Florian da Costa <florian.dacosta@akretion.com>
2017-05-17 12:31:25 +0200

    Add sale_order_count

---
+++ product_sale_count/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Akretion (<http://www.akretion.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{'name': 'Product Sale Count',
+ 'version': '8.0.1.0.0',
+ 'author': 'Akretion,Odoo Community Association (OCA)',
+ 'website': 'http://www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Sales Management',
+ 'depends': ['sale'],
+ 'data': [],
+ 'installable': True,
+ }
