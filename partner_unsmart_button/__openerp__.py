71b574e982b4cd1131c993801bb4a4e7cf38fa9e
Florian da Costa <florian.dacosta@akretion.com>
2017-07-24 20:03:55 +0200

    Add partner_unsmart_button, just with purchase and account fields for now

---
+++ partner_unsmart_button/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Akretion (<http://www.akretion.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{'name': 'Partner Unsmart Button',
+ 'version': '9.0.1.0.0',
+ 'author': 'Akretion',
+ 'website': 'http://www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'accounting',
+ 'description': 'Remove smart buttons values to gain performance on partner',
+ 'depends': ['purchase'],
+ 'data': ['views/partner_view.xml'],
+ 'installable': True,
+ }
