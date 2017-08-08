2b7ee208038b7b2054c2dd943f9313673d5bc983
David Beal <david.beal@akretion.com>
2017-08-08 17:40:33 +0200

    FIX test_check_public_price_keeper_number

--- public_price_keeper/tests/test_price_keeper.py
+++ public_price_keeper/tests/test_price_keeper.py
@@ -9,24 +9,24 @@ from openerp.tests.common import TransactionCase
 class TestPriceKeeper(TransactionCase):
 
     def test_check_public_price_keeper_number(self):
-        default_items = self.env['product.pricelist.item'].search_count(
-            [('default_item', '=', True)])
+        base_items = self.env['product.pricelist.item'].search_count(
+            [('base_item', '=', True)])
         sale_price_versions = self.env['product.pricelist.version'] \
             .search_count([('pricelist_id.type', '=', 'sale')])
         self.assertEqual(
-            default_items, sale_price_versions,
+            base_items, sale_price_versions,
             "You must have as many sale pricelist versions "
             "as base_item pricelist")
 
     def test_check_public_price_keeper_validity(self):
         items = self.env['product.pricelist.item'].search(
-            ['|',
+            ['|', '|', '|', '|',
              ('price_surcharge', '!=', 0),
              ('price_discount', '!=', 0),
              ('categ_id', '!=', False),
              ('product_id', '!=', False),
              ('product_tmpl_id', '!=', False),
-             ('default_item', '=', True),
+             ('base_item', '=', True),
              ], order='price_version_id')
         self.assertEqual(
             items, self.env['product.pricelist.item'],
