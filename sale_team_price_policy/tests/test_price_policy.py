ccf178ff04ed46626af07eb5024e0f6fb584c5c4
David Beal <david.beal@akretion.com>
2017-08-23 19:48:37 +0200

    IMP add test_sale_update_team()

--- sale_team_price_policy/tests/test_price_policy.py
+++ sale_team_price_policy/tests/test_price_policy.py
@@ -31,6 +31,11 @@ class TestPricePolicy(TransactionCase):
         self.contract_price = self.env.ref(
             'sale_team_price_policy.product_pricelist_sale_team')\
             .price_get(self.prd.id, 1)[self.contract_plist.id]
+        # sales
+        self.sale_order_partner = self.env.ref(
+            'sale_team_price_policy.sale_partner_pricelist')
+        self.sale_order_team = self.env.ref(
+            'sale_team_price_policy.sale_market_pricelist')
 
     def test_sale_price(self):
         compare = (
@@ -44,6 +49,33 @@ class TestPricePolicy(TransactionCase):
             self.assertEqual(
                 line.price_unit, pricing, LINE_MESSAGE
                 % (line, line.price_unit, pricing))
+
+    def test_sale_update_team(self):
+        # FIRST SALE
+        # intial state
+        self.assertEqual(
+            self.sale_order_partner.pricelist_id, self.partner_plist)
+        res = self.sale_order_partner.onchange_pricelist_id(
+            self.env.ref('product.list0').id,
+            self.sale_order_partner.order_line)
+        # Update pricelist raise a warning
+        self.assertNotEquals(res.get('warning', False), False,
+                             "Warning waited")
+        self.assertEqual(
+            self.sale_order_partner.pricelist_id, self.partner_plist,
+            "Pricelist not in initial value")
+        # SECOND SALE
+        self.assertEqual(
+            self.sale_order_team.pricelist_id, self.contract_plist)
+        res = self.sale_order_team.onchange_pricelist_id(
+            self.partner_plist.id,
+            self.sale_order_team.order_line)
+        # Update pricelist raise a warning
+        self.assertNotEquals(res.get('warning', False), False,
+                             "Warning waited")
+        self.assertEqual(
+            self.sale_order_team.pricelist_id, self.contract_plist,
+            "Pricelist not in initial value")
 
     def test_invoice_price(self):
         compare = (
