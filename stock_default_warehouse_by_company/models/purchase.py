edaa67d358e99cb8147b6809e72367269e897e31
chafique-delli <chafique.delli@akretion.com>
2017-11-29 16:02:24 +0100

    fix code after issue open by @david

--- stock_default_warehouse_by_company/models/purchase.py
+++ stock_default_warehouse_by_company/models/purchase.py
@@ -2,21 +2,20 @@
 # © 2017 Chafique DELLI @ Akretion <chafique.delli@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, fields
+from openerp import models, api
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     @api.model
-    def _get_picking_in(self):
+    def default_get(self, fields_list):
+        res = super(PurchaseOrder, self).default_get(fields_list)
         company = self.env.user.company_id
         if company.receipt_warehouse_id:
-            types = self.env['stock.picking.type'].search([
+            picking_types = self.env['stock.picking.type'].search([
                 ('code', '=', 'incoming'),
                 ('warehouse_id', '=', company.receipt_warehouse_id.id)])
-            if types:
-                return types[0]
-        return super(PurchaseOrder, self)._get_picking_in()
-
-    picking_type_id = fields.Many2one(default=_get_picking_in)
+            if picking_types:
+                res['picking_type_id'] = picking_types[0].id
+        return res
