21b1183f17795659a5bb711c19091b3ecfd0b547
hpar <raph@hpar.fr>
2017-11-30 17:51:21 +0100

    Add module web_search_debounce

---
+++ web_search_debounce/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_debounce',
+    "version": "10.0.0.0.1",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml',
+    ],
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "installable": True,
+}
