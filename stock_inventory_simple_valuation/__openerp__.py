d9cebe884f6579f41a2741f702090057be4cd1f9
David Beal <david.beal@akretion.com>
2018-03-01 12:35:53 +0100

    ADD stock_inventory_simple_valuation

---
+++ stock_inventory_simple_valuation/__openerp__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# © 2017 David BEAL @ Akretion <david.beal@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Inventory Valuation",
+    'summary': "... ",
+    'description': """
+        Insipred by
+    https://github.com/abakus-it/stock/tree/9.0/stock_valuation_improved
+    """,
+    'author': "Akretion",
+    'website': "http://www.akretion.com",
+    'category': 'Stock',
+    'version': '8.0.1.0.1',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/inventory_view.xml',
+    ],
+}
