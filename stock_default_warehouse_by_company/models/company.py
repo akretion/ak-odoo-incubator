4596cbfb2c6b183acd3c7815b212232634757ae0
David Beal <david.beal@akretion.com>
2018-03-09 18:58:52 +0100

    PORT V10 stock_default_warehouse_by_company

--- stock_default_warehouse_by_company/models/company.py
+++ stock_default_warehouse_by_company/models/company.py
@@ -2,7 +2,7 @@
 # © 2017 Chafique DELLI @ Akretion <chafique.delli@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
--- stock_default_warehouse_by_company/models/sale_stock.py
+++ stock_default_warehouse_by_company/models/sale_stock.py
@@ -2,7 +2,7 @@
 # © 2017 Chafique DELLI @ Akretion <chafique.delli@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
