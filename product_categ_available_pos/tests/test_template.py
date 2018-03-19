5f5dcb08f191969a173cba2832a51f0f4873cf0b
hparfr <raph@hpar.fr>
2018-03-19 15:56:39 +0100

    Fix access rights and cache invalidation

--- product_categ_available_pos/tests/test_template.py
+++ product_categ_available_pos/tests/test_template.py
@@ -20,7 +20,6 @@ class TestPosAvailableProduct(common.TransactionCase):
     def setUp(self):
         super(TestPosAvailableProduct, self).setUp()
         self.category_1_id = self.ref('product.product_category_1')
-        group_user = self.env.ref('sales_team.group_sale_manager')
         company_1 = self.env['res.company'].create(
             {'name': 'Test company 1'})
 
@@ -28,7 +27,9 @@ class TestPosAvailableProduct(common.TransactionCase):
             {'name': 'User company 1',
              'login': 'user_company_1',
              'groups_id': [
-                 (6, 0, group_user.ids)],
+                 (6, 0, self.env.ref('point_of_sale.group_pos_manager').ids),
+                 (6, 0, self.env.ref('stock.group_stock_manager').ids),
+             ],
              'company_id': company_1.id,
              'company_ids': [(6, 0, company_1.ids)]})
 
@@ -45,6 +46,10 @@ class TestPosAvailableProduct(common.TransactionCase):
         self.assertEqual(
             prod.available_in_pos, cat.available_in_pos)
         self.get_ctx(cat).set_available_in_pos(True)
+
+        self.env.invalidate_all()
+        # force clean cache to protect from false negatives
+
         self.assertEqual(
             prod.available_in_pos, cat.available_in_pos)
         self.assertTrue(prod.available_in_pos)
