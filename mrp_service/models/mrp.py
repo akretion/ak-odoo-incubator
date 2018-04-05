0c967699f0eaa9fb0f0adaa9459740600e5421c6
hparfr <raph@hpar.fr>
2018-04-05 15:09:09 +0200

    Fix bug template / product Create procurement

--- mrp_service/models/mrp.py
+++ mrp_service/models/mrp.py
@@ -19,7 +19,7 @@ class MrpBom(models.Model):
 
     # ça peut être une variante ?
     service_id = fields.Many2one(
-        'product.template', 'Service',
+        'product.product', 'Service',
         domain="[('type', 'in', ['service'])]")
 
 
@@ -27,7 +27,7 @@ class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
     service_id = fields.Many2one(
-        'product.template',
+        'product.product',
         'Service',
         readonly=True,
         compute="_compute_service_id",
@@ -40,11 +40,42 @@ class MrpProduction(models.Model):
     @api.model
     def create(self, values):
         production = super(MrpProduction, self).create(values)
-        self._generate_service_procurement()
+        production._generate_service_procurement()
         return production
 
+    @api.multi
     def _generate_service_procurement(self):
         if not self.service_id:
             return
         _logger.info('on va generer des procurements')
 
+        procurements = self.env['procurement.order']
+        for rec in self:
+            vals = rec._prepare_procurement_for_service()
+            procurements |= procurements.create(vals)
+        if procurements:
+            procurements.run()
+        import pdb
+        pdb.set_trace()
+
+
+    def _prepare_procurement_for_service(self):
+        return {
+            'name': "/",
+            'origin': self.name,
+            'company_id': self.company_id.id,
+            'date_planned': self.date_planned_start,
+            'product_id': self.service_id.id,
+            'product_qty': self.product_qty,
+            'product_uom': self.service_id.uom_po_id.id,
+            'location_id': self.location_dest_id.id,
+            'move_dest_id': (
+                self.procurement_ids and
+                self.procurement_ids[0].move_dest_id.id or False),
+            'group_id': self.procurement_group_id.id,
+            #'route_ids': [], #[(4, x.id) for x in self.route_ids],
+            'warehouse_id': (
+                self.picking_type_id and
+                self.picking_type_id.warehouse_id.id or False),
+            'priority': self.priority,
+        }
