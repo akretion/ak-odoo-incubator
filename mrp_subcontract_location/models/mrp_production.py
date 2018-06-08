99785d4a9e157d125568aa725e5a72c81ade6d3e
hparfr <raph@hpar.fr>
2018-06-08 16:39:33 +0200

    One picking for one PO

--- mrp_subcontract_location/models/mrp_production.py
+++ mrp_subcontract_location/models/mrp_production.py
@@ -15,9 +15,8 @@ _logger = logging.getLogger(__name__)
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    def update_locations(self, purchase):
+    def update_locations(self, subcontract_loc):
         self.ensure_one()
-        subcontract_loc = purchase.partner_id.supplier_location_id
         if not subcontract_loc:
             raise exceptions.ValidationError(
                 _('No location configured on the subcontractor'))
@@ -27,7 +26,7 @@ class MrpProduction(models.Model):
         self.picking_type_id = subcontract_loc.get_warehouse().manu_type_id
 
     @api.multi
-    def add_moves_near_production(self):
+    def add_moves_before_production(self, supplier_location_id, picking=False):
         """Change products locations according to mo's location
 
         Workflow
@@ -45,11 +44,11 @@ class MrpProduction(models.Model):
         new picking A_SUPPLIER:Receipts
         Add moves between Stock and Production."""
         self.ensure_one()
-        supplier_location_id = self.location_src_id
 
         # Modify incoming move and create a second one hereafter
         # It's easier to add new moves after existing ones.
-        picking_in = picking_out = False
+        moves_in = self.env['stock.move']
+        moves_prod = self.env['stock.move']
         for move_in in self.move_raw_ids:
             if move_in.location_id == supplier_location_id:
                 # don't run twice
@@ -57,18 +56,15 @@ class MrpProduction(models.Model):
             if move_in.procure_method == 'make_to_stock':
                 # prevent picking creation
                 move_in.location_id = supplier_location_id
-                continue
             elif move_in.procure_method == 'make_to_order':
-                if not picking_in:
-                    picking_in = self.create_picking_in(supplier_location_id)
                 sup_2_prod = self.new_move_just_before_prod(
                     move_source=move_in,
                     location=supplier_location_id)
                 move_in.raw_material_production_id = False
                 move_in.move_dest_id = sup_2_prod
-                move_in.picking_id = picking_in
-                move_in.location_id = picking_in.location_id
-                move_in.location_dest_id = picking_in.location_dest_id
+                move_in.location_id = picking.location_id
+                move_in.location_dest_id = picking.location_dest_id
+                move_in.picking_id = picking
 
                 # update the procurement
                 for procurement in self.procurement_group_id.procurement_ids:
@@ -76,11 +72,19 @@ class MrpProduction(models.Model):
                         procurement.move_dest_id = sup_2_prod
                         procurement.location_id = supplier_location_id
                 sup_2_prod.action_confirm()  # don't let it in draft
+                moves_prod |= sup_2_prod
             else:
                 raise exceptions.UserError(_('Unkown procure method'))
+            moves_in |= move_in
+        return moves_in, moves_prod
 
+    @api.multi
+    def add_moves_after_production(self, location, picking=False):
         # Modify outgoing move and create a second one herebefore
         # It's easier to add new move before the existing one
+        moves_out = self.env['stock.move']
+        moves_prod = self.env['stock.move']
+
         for move_out in self.move_finished_ids:
             if move_out.location_dest_id == self.location_dest_id:
                 continue
@@ -89,36 +93,21 @@ class MrpProduction(models.Model):
             if not move_out.move_dest_id:
                 _logger.info(
                     'Prod fini en make to stock. On change juste la loc')
-                move_out.location_dest_id = supplier_location_id
+                move_out.location_dest_id = location
             else:
                 _logger.info('Prod fini en make to order')
-                if not picking_out:
-                    picking_out = self.create_picking_out(supplier_location_id)
                 prod_2_sup = self.new_move_just_after_prod(
                     move_source=move_out,  # virtual/prod
-                    location=supplier_location_id)
-                _logger.info('On a cree un picking %s' % picking_out.name)
+                    location=location)
                 move_out.production_id = False
-                move_out.location_id = picking_out.location_id
-                move_out.location_dest_id = picking_out.location_dest_id
-                move_out.picking_id = picking_out
+                move_out.location_id = picking.location_id
+                move_out.location_dest_id = picking.location_dest_id
+                move_out.picking_id = picking
                 prod_2_sup.action_confirm()  # don't let it in draft
+                moves_prod |= prod_2_sup
+            moves_out |= move_out
+        return moves_out, moves_prod
 
-    def create_picking_in(self, location):
-        return self.env['stock.picking'].create({
-            'picking_type_id': location.get_warehouse().in_type_id.id,
-            'location_id': self.env.ref(
-                'stock.stock_location_inter_wh').id,
-            'location_dest_id': location.id,
-        })
-
-    def create_picking_out(self, location):
-        return self.env['stock.picking'].create({
-            'picking_type_id': location.get_warehouse().out_type_id.id,
-            'location_id': location.id,
-            'location_dest_id': self.env.ref(
-                'stock.stock_location_inter_wh').id,
-        })
 
     def _prepare_copy_move(self, move_source):
         # Existing -> New + Existing 
--- mrp_subcontract_location/models/purchase_order.py
+++ mrp_subcontract_location/models/purchase_order.py
@@ -20,14 +20,26 @@ class PurchaseOrder(models.Model):
     def button_approve(self):
         res = super(PurchaseOrder, self).button_approve()
         for purchase in self:
+            location = self._get_location()
+            picking_in = self.create_picking_in(location)
+            picking_out = self.create_picking_out(location)
             for line in purchase.order_line:
                 if line._is_service_procurement():
                     mo = line.procurement_ids.production_id
-                    mo.update_locations(purchase)
-                    mo.add_moves_near_production()
+                    mo.update_locations(location)
+                    moves_in, moves_in_prod = mo.add_moves_before_production(
+                        location, picking_in)
+                    moves_out, moves_out_prod = mo.add_moves_after_production(
+                        location, picking_out)
+
+    @api.multi
+    def _get_location(self):
+        self.ensure_one()
+        return self.partner_id.supplier_location_id
 
     @api.multi
     def _get_destination_location(self):
+        # TODO: toujours d'actu ?
         self.ensure_one()
         supplier_wh = self.env.ref(
             'mrp_subcontract_location.warehouse_supplier')
@@ -38,3 +50,19 @@ class PurchaseOrder(models.Model):
             return self.dest_address_id.supplier_location_id.id
         else:
             return super(PurchaseOrder, self)._get_destination_location()
+
+    def create_picking_in(self, location):
+        return self.env['stock.picking'].create({
+            'picking_type_id': location.get_warehouse().in_type_id.id,
+            'location_id': self.env.ref(
+                'stock.stock_location_inter_wh').id,
+            'location_dest_id': location.id,
+        })
+
+    def create_picking_out(self, location):
+        return self.env['stock.picking'].create({
+            'picking_type_id': location.get_warehouse().out_type_id.id,
+            'location_id': location.id,
+            'location_dest_id': self.env.ref(
+                'stock.stock_location_inter_wh').id,
+        })
