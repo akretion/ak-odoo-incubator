a9528ad9967d0a1040b24e3bcc160f9bdf49c129
David Beal <david.beal@akretion.com>
2018-06-26 17:40:42 +0200

    PORT sale_price_origin to v10

--- sale_price_origin/models/sale.py
+++ sale_price_origin/models/sale.py
@@ -2,7 +2,7 @@
 # © 2017 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
