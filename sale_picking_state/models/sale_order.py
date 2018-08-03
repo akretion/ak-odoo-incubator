d8260c393cd2ea818955728307cb0624d8d64d54
Pierrick Brun <brun.pierrick@protonmail.com>
2018-08-03 17:25:11 +0200

    [FIX] make picking_state field not computed

--- sale_picking_state/models/sale_order.py
+++ sale_picking_state/models/sale_order.py
@@ -3,7 +3,7 @@
 # @author Pierrick BRUN <pierrick.brun@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class SaleOrder(models.Model):
@@ -11,23 +11,9 @@ class SaleOrder(models.Model):
 
     picking_state = fields.Selection([
         ('unprocessed', 'Unprocessed'),
-        ('partially', 'Partially'),
+        ('partially', 'Partially processed'),
         ('done', 'Done')],
-        compute='_compute_picking_state',
         string='Picking state',
-
+        copy=False,
+        default='unprocessed'
     )
-
-    @api.multi
-    @api.depends('picking_ids')
-    def _compute_picking_state(self):
-        for order in self:
-            states = set()
-            for picking in order.picking_ids:
-                states.add(picking.state)
-            if 'done' not in states:
-                order.picking_state = 'unprocessed'
-            elif len(states) == 1:
-                order.picking_state = 'done'
-            else:
-                order.picking_state = 'partially'
