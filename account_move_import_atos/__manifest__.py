40051bf47c4ff0ae94898085578476ff85fca595
Pierrick Brun <brun.pierrick@protonmail.com>
2018-08-14 13:04:50 +0200

    [ADD] account_move_import_atos module

---
+++ account_move_import_atos/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012-2018 Akretion (http://www.akretion.com).
+# @author Benoît GUILLOT <benoit.guillot@akretion.com>
+# @author Pierrick BRUN <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Import Atos",
+    "summary": "Allows to import journal entries from Atos .xls files",
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "depends": ["account_move_base_import"],
+    "data": [],
+    "installable": True,
+}
---
+++ account_move_import_atos/parser/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import atos_parser
