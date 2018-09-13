c452e93d8607e27efda000dfb66528d4ddb6c78f
hparfr <raph@hpar.fr>
2018-09-13 10:26:51 +0200

    Set planned date to today + dlt

--- stock_orderpoint_traffic_lights/models/make_procurement.py
+++ stock_orderpoint_traffic_lights/models/make_procurement.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
 from odoo import api, fields, models
 
 
@@ -10,3 +11,18 @@ class MakeProcurement(models.TransientModel):
 
     dlt = fields.Float(
         'Decoupled Lead Time', related='product_id.dlt')
+
+    @api.onchange('product_id')
+    def _calc_date_planned(self):
+        '''Use minimal viable date.
+
+        Dlt + today is the sooner date we can propose.
+        It's not a default func because product_id not set
+        on load.
+        '''
+        today = datetime.today()
+        delta = relativedelta(days=int(self.product_id.dlt))
+        res = fields.Date.to_string(
+            today + delta
+        )
+        self.date_planned = res
