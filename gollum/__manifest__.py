bc568cd6dcbf29b9d75d6d7142ea2271d75c0185
Pierrick Brun <pierrick.brun@akretion.com>
2018-09-19 12:10:14 +0200

    [MIG] migrate odoo-gollum to v10
    Also allow to upload files via the odoo proxy

--- gollum/__manifest__.py
+++ gollum/__manifest__.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Akretion (http://www.akretion.com).
+# Copyright 2017-2018 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
+# @author Pierrick Brun <pierrick.brun@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     "name": "Gollum wiki integration",
     "summary": "Gollum wiki integration",
-    "version": "8.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Uncategorized",
     "website": "www.akretion.com",
     "author": " Akretion",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
     "depends": [
         "base",
     ],
@@ -25,9 +22,5 @@
         'security/group.xml',
         'data/config.xml',
         'views/menu.xml',
-    ],
-    "demo": [
-    ],
-    "qweb": [
     ]
 }
--- gollum/controllers/main.py
+++ gollum/controllers/main.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Akretion (http://www.akretion.com)
 # Sébastien BEAU <sebastien.beau@akretion.com>
+# Pierrick Brun <pierrick.brun@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from werkzeug.exceptions import Unauthorized
-from openerp.http import request
-from openerp import http
+from werkzeug.datastructures import FileStorage
+from odoo.http import request
+from odoo import http
 import requests
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from unidecode import unidecode
+except:
+    _logger.debug('Cannot `import unidecode`.')
 
 
 class GollumController(http.Controller):
@@ -16,7 +26,7 @@ class GollumController(http.Controller):
         return http.local_redirect('/wiki/home')
 
     @http.route('/wiki/<path:path>', methods=['GET', 'DELETE', 'PUT', 'POST'],
-                auth="user")
+                auth="user", csrf=False)
     def proxy(self, *args, **kwargs):
         method = request.httprequest.method
         base_path = request.env['ir.config_parameter'].get_param('gollum_path')
@@ -28,13 +38,21 @@ class GollumController(http.Controller):
             access = 'read'
         headers = {
             'access': access,
-            'user': user.name,
+            'user': unidecode(user.name),
             'email': user.email,
             }
+        post_files = {}
+        for k, v in kwargs.copy().iteritems():
+            if isinstance(v, FileStorage):
+                p_file = kwargs.pop(k)
+                post_files[k] = (
+                    p_file.filename, p_file.stream,
+                    p_file.mimetype, headers)
         if method == 'GET':
             res = requests.get(url, data=kwargs, headers=headers)
         elif method == 'POST' and access == 'edit':
-            res = requests.post(url, data=kwargs, headers=headers)
+            res = requests.post(
+                url, data=kwargs, headers=headers, files=post_files)
         elif method == 'DELETE' and access == 'edit':
             res = requests.delete(url, data=kwargs, headers=headers)
         else:
