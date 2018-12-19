31bb9154cc0a261d86948adadd5d794826af15d2
Pierrick brun <pierrick.brun@akretion.com>
2018-12-19 13:23:18 +0100

    it is LGPL and not AGPL

--- stock_picking_wave_report/__manifest__.py
+++ stock_picking_wave_report/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # © 2013-2017 Odoo SA
 # © 2018 Pierrick Brun <pierrick.brun@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Stock Picking Wave Report',
     'version': '10.0.1.0.0',
     'category': 'Warehouse',
     'author': 'Odoo SA, Akretion, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'depends': ['stock_picking_wave'],
     'data': [
         'report/stock_picking_wave_report_views.xml',
