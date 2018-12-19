6a8d34f1dfcf50461d922118682e890cbe6d4eb5
Pierrick brun <pierrick.brun@akretion.com>
2018-12-19 13:17:25 +0100

    [FIX] flake8

--- stock_picking_wave_report/models/stock_picking_wave.py
+++ stock_picking_wave_report/models/stock_picking_wave.py
@@ -7,11 +7,10 @@ from odoo.exceptions import UserError
 class StockPickingWave(models.Model):
     _inherit = "stock.picking.wave"
 
-
     @api.multi
     def print_picking(self):
         pickings = self.mapped('picking_ids')
         if not pickings:
             raise UserError(_('Nothing to print.'))
-        return self.env["report"].get_action(self, 'stock_picking_wave_report.report_picking_wave')
-
+        return self.env["report"].get_action(
+                self, 'stock_picking_wave_report.report_picking_wave')
