2749d1d663c017254bf57ba6bb9c39d908f0e727
mourad-ehm <mourad.elhadj.mimoune@akretion.com>
2018-12-21 10:07:05 +0100

    Fix flack8

--- product_categ_available_pos/__manifest__.py
+++ product_categ_available_pos/__manifest__.py
@@ -3,7 +3,8 @@
     'name': 'Product category available in pos by company',
     'version': '10.0.0.1.1',
     'category': 'Point Of Sale',
-    'summary': "Set company_dependent to product.category.available_in_pos. Allows search from category",
+    'summary': """Set company_dependent to product.category.available_in_pos.
+        Allows search from category""",
     'author': 'Akretion',
     'website': 'https://akretion.com',
     "license": "AGPL-3",
