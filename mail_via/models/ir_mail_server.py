aa2f690b48d1b39749f6acb73d452f77dd09c3a3
Sébastien BEAU <sebastien.beau@akretion.com>
2019-01-10 19:32:56 +0100

    [ADD] add module mail via to solve issue when sending email with smtp like mailjet that filter the from of the message

---
+++ mail_via/models/ir_mail_server.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class IrMail_Server(models.Model):
+    _inherit = 'ir.mail_server'
+
+    def build_email(self, email_from, email_to, subject, body, **kwargs):
+        if self._context.get('sender_is_via'):
+            via = self.env['ir.config_parameter'].get_param("mail.via.alias")
+            domain = self.env['ir.config_parameter'].get_param(
+                    "mail.catchall.domain")
+            original_from = email_from.replace('<', '"').replace('>', '"')
+            email_from = '%s <%s@%s>' % (original_from, via, domain)
+        return super(IrMail_Server, self).build_email(
+            email_from, email_to, subject, body, **kwargs)
+
