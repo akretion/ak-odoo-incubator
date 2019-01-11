2d707debd33ac58bf30cf92b8c0a110867e0c9db
Sébastien BEAU <sebastien.beau@akretion.com>
2019-01-11 17:16:35 +0100

    [PEP] clean pep8

--- mail_via/models/ir_mail_server.py
+++ mail_via/models/ir_mail_server.py
@@ -3,7 +3,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from odoo import models
 
 
 class IrMail_Server(models.Model):
@@ -13,9 +13,8 @@ class IrMail_Server(models.Model):
         if self._context.get('sender_is_via'):
             via = self.env['ir.config_parameter'].get_param("mail.via.alias")
             domain = self.env['ir.config_parameter'].get_param(
-                    "mail.catchall.domain")
+                "mail.catchall.domain")
             original_from = email_from.replace('<', '"').replace('>', '"')
             email_from = '%s <%s@%s>' % (original_from, via, domain)
         return super(IrMail_Server, self).build_email(
             email_from, email_to, subject, body, **kwargs)
-
--- mail_via/models/mail_mail.py
+++ mail_via/models/mail_mail.py
@@ -3,7 +3,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from odoo import models
 
 
 class MailMail(models.Model):
@@ -12,7 +12,7 @@ class MailMail(models.Model):
     def send(self, auto_commit=False, raise_exception=False):
         incomming_mails = self.filtered('fetchmail_server_id')
         super(MailMail, incomming_mails.with_context(sender_is_via=True)).\
-             send(auto_commit=auto_commit, raise_exception=raise_exception)
+            send(auto_commit=auto_commit, raise_exception=raise_exception)
         normal_mails = self.filtered(lambda s: not s.fetchmail_server_id)
         super(MailMail, normal_mails).send(
             auto_commit=auto_commit, raise_exception=raise_exception)
