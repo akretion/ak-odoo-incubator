2f76fd066a238e8b238d548bc0e4d1fa749e7513
David Beal <david.beal@akretion.com>
2019-02-15 13:03:52 +0100

    FIX according Florian advices. Thanks

--- mrp_production_replace_component/wizard/wiz_replace_component.py
+++ mrp_production_replace_component/wizard/wiz_replace_component.py
@@ -8,9 +8,14 @@ from odoo.exceptions import UserError
 class ReplaceComponent(models.TransientModel):
     _name = 'replace.component.transient'
 
+    production_id = fields.Many2one(
+        comodel_name='mrp.production', required=True,
+        default=lambda s: s.env.context.get('active_id'))
     remove_product_ids = fields.Many2many(
-        comodel_name='product.product', string='Products',
-        domain=lambda self: self._get_remove_product_ids_domain())
+        comodel_name='product.product', string='Products')
+    remove_product_domain_ids = fields.Many2many(
+        comodel_name='product.product', compute='_compute_remove_prd_domain',
+        help="Only required to store domain of available removable products.")
     product_id = fields.Many2one(
         comodel_name='product.product', string='Product',
         domain=[('bom_line_ids', '!=', False)])
@@ -18,15 +23,11 @@ class ReplaceComponent(models.TransientModel):
     product_uom_id = fields.Many2one(
         comodel_name='uom.uom', string='Unit', readonly=True)
 
-    @api.model
-    def _get_remove_product_ids_domain(self):
-        res = []
-        mo = self._get_manuf_order()
-        if mo:
-            # transient may use falsy value
-            mo.invalidate_cache()
-            res = [('id', 'in', [x.product_id.id for x in mo.move_raw_ids])]
-        return res
+    @api.depends('production_id.move_raw_ids')
+    def _compute_remove_prd_domain(self):
+        prd_ids = [x.product_id.id for x in self.production_id.move_raw_ids]
+        for rec in self:
+            rec.remove_product_domain_ids = prd_ids
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
@@ -41,79 +42,68 @@ class ReplaceComponent(models.TransientModel):
                 messages.append(_("Component %s removed" % remove_mess))
         if self.product_id:
             self._append_raw_materials()
-            messages.append(_("Component %s added" % self.product_id.name))
+            messages.append(_("Component '%s' added" % self.product_id.name))
+        self.production_id._adjust_procure_method()
+        self.production_id.move_raw_ids._action_confirm()
         if messages:
-            mo = self._get_manuf_order()
-            mo.message_post(' - '.join(messages))
+            self.production_id.message_post(' - '.join(messages))
         return {'type': 'ir.actions.act_window_close'}
 
     def _append_raw_materials(self):
         self.ensure_one()
-        mo = self._get_manuf_order()
-        if mo:
-            routing = False
-            if mo.routing_id:
-                routing = self.routing_id
-            if routing and routing.location_id:
-                source_location = routing.location_id
-            else:
-                source_location = mo.location_src_id
-            bom_line = self.env['mrp.bom.line'].search(
-                [('product_id', '=', self.product_id.id)], limit=1)
-            if not bom_line:
-                raise UserError(
-                    _('Additional raw material must be defined in a bom'))
-            data = {
-                'sequence': 100,
-                'name': mo.name,
-                'date': mo.date_planned_start,
-                'date_expected': mo.date_planned_start,
-                'picking_type_id': mo.picking_type_id.id,
-                'product_id': self.product_id.id,
-                'product_uom_qty': self.product_uom_qty,
-                'product_uom': self.product_id.uom_id.id,
-                'location_id': source_location.id,
-                'location_dest_id':
-                    self.product_id.property_stock_production.id,
-                'raw_material_production_id': mo.id,
-                'company_id': mo.company_id.id,
-                'price_unit': self.product_id.standard_price,
-                'procure_method': 'make_to_stock',
-                'origin': mo.name,
-                'warehouse_id': source_location.get_warehouse().id,
-                'group_id': mo.procurement_group_id.id,
-                'propagate': mo.propagate,
-                'state': 'confirmed',
-                # required to be consumed by produce wizard
-                'bom_line_id': bom_line.id,
-                # only one supported
-                'unit_factor': 1,
-            }
-            self.env['stock.move'].create(data)
+        routing = False
+        if self.production_id.routing_id:
+            routing = self.routing_id
+        if routing and routing.location_id:
+            source_location = routing.location_id
+        else:
+            source_location = self.production_id.location_src_id
+        bom_line = self.env['mrp.bom.line'].search(
+            [('product_id', '=', self.product_id.id)], limit=1)
+        if not bom_line:
+            raise UserError(
+                _('Additional raw material must be defined in a bom'))
+        data = {
+            'sequence': 100,
+            'name': self.production_id.name,
+            'date': self.production_id.date_planned_start,
+            'date_expected': self.production_id.date_planned_start,
+            'picking_type_id': self.production_id.picking_type_id.id,
+            'product_id': self.product_id.id,
+            'product_uom_qty': self.product_uom_qty,
+            'product_uom': self.product_id.uom_id.id,
+            'location_id': source_location.id,
+            'location_dest_id':
+                self.product_id.property_stock_production.id,
+            'raw_material_production_id': self.production_id.id,
+            'company_id': self.production_id.company_id.id,
+            'price_unit': self.product_id.standard_price,
+            'procure_method': 'make_to_stock',
+            'origin': self.production_id.name,
+            'warehouse_id': source_location.get_warehouse().id,
+            'group_id': self.production_id.procurement_group_id.id,
+            'propagate': self.production_id.propagate,
+            'state': 'draft',
+            # required to be consumed by produce wizard
+            'bom_line_id': bom_line.id,
+            # only one supported
+            'unit_factor': 1,
+        }
+        self.env['stock.move'].create(data)
 
     @api.model
     def _remove_raw_materials(self):
-        mo = self._get_manuf_order()
-        if mo:
-            moves = self.env['stock.move'].search(
-                [('raw_material_production_id', '=', mo.id),
-                 ('product_id', 'in', self.remove_product_ids.ids)])
-            moves.write({'state': 'draft'})
-            move_lines = self.env['stock.move.line'].search(
-                [('move_id', 'in', moves.ids)])
-            component_names = [x.name for x in self.remove_product_ids]
-            if move_lines:
-                raise UserError(_(
-                    "Products '%s' are not available for deletion "
-                    "as raw material" % component_names))
-            else:
-                moves.unlink()
-                return component_names
-        return False
-
-    @api.model
-    def _get_manuf_order(self):
-        if self.env.context.get('active_model') == 'mrp.production':
-            return self.env['mrp.production'].browse(
-                self.env.context.get('active_id'))
+        moves = self.env['stock.move'].search(
+            [('raw_material_production_id', '=', self.production_id.id),
+             ('product_id', 'in', self.remove_product_ids.ids)])
+        move_lines = moves.mapped('move_line_ids')
+        component_names = [x.name for x in self.remove_product_ids]
+        if move_lines:
+            raise UserError(_(
+                "Products '%s' are not available for deletion "
+                "as raw material" % component_names))
+        else:
+            moves._action_cancel()
+            moves.unlink()
+            return component_names
         return False
