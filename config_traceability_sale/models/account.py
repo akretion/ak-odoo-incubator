662833f277ab64a92ab9ecfba7441ef95b177c54
David Beal <david.beal@akretion.com>
2019-04-11 14:38:54 +0200

    FEAT account.tax chatter

--- config_traceability_sale/models/account.py
+++ config_traceability_sale/models/account.py
@@ -7,6 +7,11 @@ from odoo import models, api, fields
 """ Mail parameters from odoo documentation:
      - ``mail_create_nosubscribe``: at create or message_post, do not subscribe
        uid to the record thread
+
+    We want to be able to send custom messages in these objects
+    but we don't want, by default, automatic messages
+    created by odoo core.
+
 """
 
 CTX = {
@@ -15,10 +20,6 @@ CTX = {
 
 
 class AccountAccount(models.Model):
-    """ We want to be able to send custom messages in this object
-        but we don't want, by default, automatic messages
-        created by odoo core.
-    """
     _name = 'account.account'
     _inherit = ['account.account', 'mail.thread']
 
@@ -33,3 +34,25 @@ class AccountAccount(models.Model):
 
     def write(self, vals):
         return super(AccountAccount, self.with_context(CTX)).write(vals)
+
+
+class AccountTax(models.Model):
+    """ We want to be able to send custom messages in this object
+        but we don't want, by default, automatic messages
+        created by odoo core.
+    """
+    _name = 'account.tax'
+    _inherit = ['account.tax', 'mail.thread']
+
+    name = fields.Char(track_visibility='onchange')
+    price_include = fields.Boolean(track_visibility='onchange')
+    type_tax_use = fields.Selection(track_visibility='onchange')
+    amount_type = fields.Selection(track_visibility='onchange')
+    amount = fields.Float(track_visibility='onchange')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        return super(AccountTax, self.with_context(CTX)).create(vals_list)
+
+    def write(self, vals):
+        return super(AccountTax, self.with_context(CTX)).write(vals)
