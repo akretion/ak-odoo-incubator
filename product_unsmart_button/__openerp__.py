c18186f0e7037f259edf8c9a3793ed39a5d9f4b9
Florian da Costa <florian.dacosta@akretion.com>
2019-04-18 21:27:40 +0200

    Migrate product_unsmart_button to v9

--- product_unsmart_button/__openerp__.py
+++ product_unsmart_button/__openerp__.py
@@ -3,8 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {'name': 'Product Unsmart Button',
- 'version': '8.0.1.0.0',
- 'author': 'Akretion,Odoo Community Association (OCA)',
+ 'version': '9.0.1.0.0',
+ 'author': 'Akretion',
  'website': 'http://www.akretion.com',
  'license': 'AGPL-3',
  'category': 'Sales',
