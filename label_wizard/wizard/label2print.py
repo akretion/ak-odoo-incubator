68ee8e71306e9bbe7c49a831f01eaa575acd9379
Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
2019-04-30 10:25:03 +0200

    [FIX] rebase conflict on proxy_action/models/helper.py & black formating

--- label_wizard/wizard/label2print.py
+++ label_wizard/wizard/label2print.py
@@ -2,60 +2,69 @@
 # © 2016 David BEAL @ Akretion <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import Warning as UserError
 
 
 class LabelFromRecord(models.TransientModel):
-    _name = 'label.from.record'
+    _name = "label.from.record"
 
     @api.model
     def __get_label_content(self):
         return self._get_label_content()
 
     content = fields.Text(
-        string=u"Label's content", default=__get_label_content)
+        string=u"Label's content", default=__get_label_content
+    )
     with_price = fields.Boolean(
-        string=u"Print price",
-        help=u"Print price on labels")
+        string=u"Print price", help=u"Print price on labels"
+    )
 
     @api.model
     def _get_label_content(self):
         product_infos = []
-        model = self._context['active_model']
-        if model == 'stock.picking':
+        model = self._context["active_model"]
+        if model == "stock.picking":
             # PICKING
-            if self._context.get('active_id'):
-                moves = self.env['stock.move'].search([
-                    ('picking_id', '=', self._context['active_id'])])
+            if self._context.get("active_id"):
+                moves = self.env["stock.move"].search(
+                    [("picking_id", "=", self._context["active_id"])]
+                )
                 product_infos = [
-                    '%s ; %s ; %s' % (
-                        x.product_id.default_code or '_',
+                    "%s ; %s ; %s"
+                    % (
+                        x.product_id.default_code or "_",
                         int(x.product_uom_qty),
-                        x.product_id.id)
-                    for x in moves if x.product_id]
+                        x.product_id.id,
+                    )
+                    for x in moves
+                    if x.product_id
+                ]
         else:
             # PRODUCT
-            products = self.env[model].browse(self._context['active_ids'])
-            product_infos = ['%s ; %s ; %s' % (
-                             x.default_code or '_', 1, x.id)
-                             for x in products]
-        return '\n'.join(product_infos)
+            products = self.env[model].browse(self._context["active_ids"])
+            product_infos = [
+                "{} ; {} ; {}".format(x.default_code or "_", 1, x.id)
+                for x in products
+            ]
+        return "\n".join(product_infos)
 
     @api.multi
     def generate_label(self):
-        Product_m = self.env['product.product']
+        Product_m = self.env["product.product"]
         for rec in self:
             if rec.content:
                 products2print, data4print = [], []
-                lines = rec.content.split('\n')
+                lines = rec.content.split("\n")
                 for line in lines:
                     # on remplace le text par des entiers quand possible
                     # 0 est considéré comme False donc n'est pas
                     # transformé en entier
-                    parts = [(x.strip().isdigit() and int(x.strip()) or
-                              x.strip())
-                             for x in line.split(';') if x.strip()]
+                    parts = [
+                        (x.strip().isdigit() and int(x.strip()) or x.strip())
+                        for x in line.split(";")
+                        if x.strip()
+                    ]
                     rec._sanitize_and_check_parts(parts, line)
                     products2print.append(parts[:])
                 for info in products2print:
@@ -64,21 +73,21 @@ class LabelFromRecord(models.TransientModel):
                         data4print.append((product, quantity))
                 if data4print:
                     return Product_m.get_labels_zebra(
-                        data4print,
-                        with_price=rec.with_price)
-        return {'type': 'ir.actions.act_window_close'}
+                        data4print, with_price=rec.with_price
+                    )
+        return {"type": "ir.actions.act_window_close"}
 
     @api.model
     def _search_product(self, info):
         """ info[0]: code, info[1]: quantity, info[2]: id
             return product, quantity
         """
-        Product_m = self.env['product.product']
+        Product_m = self.env["product.product"]
         if len(info) > 2:
-            product = Product_m.search([('id', '=', info[2])])
+            product = Product_m.search([("id", "=", info[2])])
             if product:
                 return (Product_m.browse(info[2]), info[1])
-        products = Product_m.search([('default_code', '=', info[0])])
+        products = Product_m.search([("default_code", "=", info[0])])
         if products:
             return (product[0], info[1])
         return (False, False)
