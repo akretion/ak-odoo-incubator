537549c76997b7a459b323d6c6e6898fa40df563
Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
2019-04-30 10:25:03 +0200

    [FIX] rebase conflict on proxy_action/models/helper.py & black formating

--- label_wizard/__manifest__.py
+++ label_wizard/__manifest__.py
@@ -1,16 +1,12 @@
 # coding: utf-8
 {
-    'name': 'Labels wizard',
-    'description': 'Wizard for printing zebra labels',
-    'version': '10.0.0.1',
-    'license': 'AGPL-3',
-    'author': "Akretion",
-    'category': 'Custom',
-    'summary': 'Wizard for choosing how many labels to print',
-    'depends': [
-        'proxy_action',
-    ],
-    'data': [
-        'wizard/wizard_view.xml',
-    ],
+    "name": "Labels wizard",
+    "description": "Wizard for printing zebra labels",
+    "version": "10.0.0.1",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "category": "Custom",
+    "summary": "Wizard for choosing how many labels to print",
+    "depends": ["proxy_action"],
+    "data": ["wizard/wizard_view.xml"],
 }
