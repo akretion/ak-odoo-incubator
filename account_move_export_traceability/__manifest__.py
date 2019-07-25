a67fc2dbd3ee6675c0d1ae0391ba776dd67da358
David Beal <david.beal@akretion.com>
2019-07-25 14:36:48 +0200

    ADD account_move_export_traceability

---
+++ account_move_export_traceability/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2019 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Accounting Move Export Traceability",
+    "version": "12.0.0.1.0.1",
+    "author": "Akretion",
+    "category": "account",
+    "depends": [
+        "account_export_csv",
+    ],
+    "website": "http://www.akretion.com",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/move_export_view.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ account_move_export_traceability/models/export.py
@@ -0,0 +1,57 @@
+# © 2019 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountMoveExport(models.Model):
+    _name = "account.move.export"
+    _description = "Account Moves Export"
+
+    name = fields.Char(required=True)
+    file = fields.Many2one(
+        comodel_name="ir.attachment", compute="_compute_attachment")
+    company_id = fields.Many2one(
+        comodel_name="res.company", string="Company")
+
+    def _compute_attachment(self):
+        attachm = self.env['ir.attachment'].search([
+            ("res_id", "in", self.ids), ("res_model", "=", self._name)])
+        mapping = {x.res_id: x.id for x in attachm}
+        for rec in self:
+            rec.file = mapping.get(rec.id)
+
+    def action_goto_related_account_move(self):
+        self.ensure_one()
+        return {
+            "name": self.name,
+            "type": "ir.actions.act_window",
+            "res_model": "account.move",
+            "view_mode": "tree,form",
+            "domain": [("export_id", "=", self.id)],
+            "target": "current",
+        }
+
+    def _link_account_moves(self, move_ids):
+        self.ensure_one()
+        moves = self.env['account.move'].browse(move_ids)
+        moves.write({"export_id": self.id})
+
+    def _create_attachment(self, data, name=None):
+        self.ensure_one()
+        self.env["ir.attachment"].create({
+            "datas": data.read(),
+            "name": name or self.name,
+            "res_id": self.id,
+            "res_model": "account.move.export",
+            "datas_fname": "%s.csv" % self.name,
+            "mimetype": "text/csv",
+            "type": "binary",
+        })
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    export_id = fields.Many2one(
+        comodel_name="account.move.export", string="Export")
