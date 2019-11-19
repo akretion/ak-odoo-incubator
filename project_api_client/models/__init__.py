1ae01056e4ee2384643cc404d022a39b494f47f6
Sébastien BEAU <sebastien.beau@akretion.com>
2019-11-19 18:18:39 +0100

    [REMOVE] module is now at https://github.com/akretion/support

--- project_api_client/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import action
-from . import menu
-from . import keychain
-from . import project
-from . import partner
--- project_api_client/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# TODO FIXME
-#from . import test_task
