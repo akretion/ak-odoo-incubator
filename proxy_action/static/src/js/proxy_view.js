efcaea391b6030cf4afa5b2d06e90576c1ef6133
Kevin Khao <kevinkhao@gmail.com>
2020-02-28 11:42:33 +0100

    [IMP] Concise proxy action notification instead of 1 notification per action sent

--- proxy_action/static/src/js/proxy_view.js
+++ proxy_action/static/src/js/proxy_view.js
@@ -7,23 +7,30 @@ odoo.define('proxy_action.proxy_view', function (require) {
         ir_actions_act_proxy: function (action, options) {
             var self = this;
             self.do_notify(_t('Proxy action executing'), _t('Your action is being executed'));
+
+            var action_success = true;
             action.action_list.forEach(function (task) {
                 $.ajax({
                     url: task['url'],
                     type: 'POST',
                     data: JSON.stringify(task['params']),
                     contentType: 'application/json',
+                    async: false,
                 }).done(function (result) {
-                    console.log("Proxy action has been successfully sent: ", result);
-                    self.do_notify(_t('Success'), _t('Proxy action successfully sent'));
-                    self.ir_actions_act_window_close(action, options);
+                    console.log("Individual proxy action has been successfully sent: ", result);
                 }).fail(function (result) {
-                    console.log('Proxy action has failed: ', result);
-                    self.do_warn(_t("Failure"), _t("Proxy action failure. Please check logs."));
-                    self.ir_actions_act_window_close(action, options);
+                    console.log('Individual proxy action has failed: ', result);
+                    action_success = false;
                 })
             })
-            this.do_action({"type":"ir.actions.act_window_close"});
+            if (action_success === true){
+                self.do_notify(_t('Success'), _t('Proxy action successfully sent.'));
+            }
+            else{
+                self.do_warn(_t("Failure"), _t("Proxy action failure. Please check logs."));
+            }
+
+            this.do_action({"type": "ir.actions.act_window_close"});
         }
     });
 });
