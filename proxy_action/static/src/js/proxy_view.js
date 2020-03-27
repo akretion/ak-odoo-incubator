4111a4b8d62088822e94c962bac7e61a4a39af95
kevinkhao <kevin.khao@akretion.com.br>
2020-03-27 04:16:51 +0100

    fixup! JS Improvement

--- proxy_action/static/src/js/proxy_view.js
+++ proxy_action/static/src/js/proxy_view.js
@@ -18,33 +18,22 @@ odoo.define('proxy_action.proxy_view', function(require) {
             self.do_notify(_t('Proxy action executing'), _t('Your action is being executed'));
             var action_success = true;
 
-            $.when(_.map(action.action_list, function(task, idx) {
+            action.action_list.map(function(act, idx) {
                 $.ajax({
-                    url: task['url'],
+                    url: act['url'],
                     type: 'POST',
-                    data: JSON.stringify(task['params']),
+                    data: JSON.stringify(act['params']),
                     contentType: 'application/json',
-                }).done(function(result) {
-                    console.log('Proxy action sent with success: ', result);
-                    return true;
                 }).fail(function(result) {
                     console.log('Proxy action has failed: ', result);
                     self.do_warn(_t("Failure"), _t("Proxy action failure. Please check logs."));
-                    return false;
+                    return result;
                 })
-            })).then(function(requests) {
-                var notif = requests.reduce(function(success, request) {
-                    if (request != true) {
-                        success = false;
-                    }
-                    return success;
-                }, true);
-                console.log(notif);
             });
-            action = {
+            var act_close = {
                 type: 'ir.actions.act_window_close'
             };
-            return self.doAction(action, []);
+            return self.doAction(act_close, []);
         },
     });
 
