f27a86b4376acbce2db4cf2fb046cba69220a9e5
Kevin.roche <kevin.roche@akretion.com>
2020-12-21 17:49:12 +0100

    blacked

--- forbid_record_creation/__manifest__.py
+++ forbid_record_creation/__manifest__.py
@@ -2,16 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Forbid Record Creation',
-    'summary': 'Avoid to create test data in sale, purchase',
-    'license': 'AGPL-3',
-    'version': '14.0.0.0.0',
-    'author': 'Akretion',
-    'maintainer': 'Akretion',
-    'category': 'base',
-    'depends': [
-        'base',
+    "name": "Forbid Record Creation",
+    "summary": "Avoid to create test data in sale, purchase",
+    "license": "AGPL-3",
+    "version": "14.0.0.0.0",
+    "author": "Akretion",
+    "maintainer": "Akretion",
+    "category": "base",
+    "depends": [
+        "base",
     ],
-    'website': 'http://www.akretion.com/',
-    'installable': True,
+    "website": "http://www.akretion.com/",
+    "installable": True,
 }
--- forbid_record_creation/models/abstract_model.py
+++ forbid_record_creation/models/abstract_model.py
@@ -1,28 +1,27 @@
 # © 2019 Akretion Mourad EL HADJ MIMOUNE, David BEAL, Raphaël REVERDY
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, api, _
+from odoo import _, api, models
 from odoo.exceptions import UserError
-
 
 # TODO use _register_hook() instead of this hack to code an independant module
 
 
 class ForbiddenModel(models.AbstractModel):
-    _name = 'forbidden.model'
-    _description = 'Prevent order creation'
+    _name = "forbidden.model"
+    _description = "Prevent order creation"
 
     @api.model
     def _my_integrator(self):
         """ Override with your own brand"""
-        return 'Akretion'
+        return "Akretion"
 
     @api.model
     def _prevent_orders(self):
-        """ You may inherit this method according to 'web.base.url' like here
-            url = self.env['ir.config_parameter'].get_param('web.base.url')
-            if url[-19:] == '/my_test_instance.com':
-                return False
+        """You may inherit this method according to 'web.base.url' like here
+        url = self.env['ir.config_parameter'].get_param('web.base.url')
+        if url[-19:] == '/my_test_instance.com':
+            return False
         """
         return True
 
@@ -30,7 +29,9 @@ class ForbiddenModel(models.AbstractModel):
     def create(self, vals):
         if self._prevent_orders():
             raise UserError(
-                _("You're not allowed to create '%s' on this database.\n\n"
-                  "Your integrator, %s"
-                  % (self._description, self._my_integrator())))
+                _(
+                    "You're not allowed to create '%s' on this database.\n\n"
+                    "Your integrator, %s" % (self._description, self._my_integrator())
+                )
+            )
         return super().create(vals)
