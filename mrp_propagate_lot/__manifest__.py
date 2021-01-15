5616942944714bdf4745ae56b7038de999bc74c2
Florian da Costa <florian.dacosta@akretion.com>
2021-01-15 23:32:35 +0100

    Add mrp_propagate_lot

---
+++ mrp_propagate_lot/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP Propagate Lot",
+    "version": "14.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/ak-odoo-incubator",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "sale_order_lot_generator",
+    ],
+    "installable": True,
+}
