96cd6ed284f0539a5da5943981a806d1e279d592
Florian da Costa <florian.dacosta@akretion.com>
2021-01-18 16:11:07 +0100

    [IMP] production_from_config: black, isort, prettier

--- production_from_config/sale.py
+++ production_from_config/sale.py
@@ -1,15 +1,14 @@
-# coding: utf-8
 # © 2016 David BEAL @ Akretion <david.beal@akretion.com>
 #        Sébastien Beau <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, fields
 import simplejson
+from openerp import api, fields, models
 
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.multi
     def _get_configuration(self):
@@ -23,22 +22,21 @@ class SaleOrderLine(models.Model):
                 line.config = simplejson.loads(self.config_text)
 
     config = fields.Serialized(
-        'Configuration',
-        readonly=True,
-        help="Allow to set custom configuration")
+        "Configuration", readonly=True, help="Allow to set custom configuration"
+    )
     config_text = fields.Text(
-        compute='_get_configuration',
-        inverse='_set_configuration',
-        string='Configuration')
+        compute="_get_configuration",
+        inverse="_set_configuration",
+        string="Configuration",
+    )
 
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.model
     def _prepare_vals_lot_number(self, order_line, index_lot):
-        res = super(SaleOrder, self)._prepare_vals_lot_number(
-            order_line, index_lot)
-        res['config'] = order_line.config
+        res = super(SaleOrder, self)._prepare_vals_lot_number(order_line, index_lot)
+        res["config"] = order_line.config
         return res
