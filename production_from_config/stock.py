bac14b1dfde80b62acbed7269138a44509108db3
Florian da Costa <florian.dacosta@akretion.com>
2021-01-18 16:11:07 +0100

    [IMP] production_from_config: black, isort, prettier

--- production_from_config/stock.py
+++ production_from_config/stock.py
@@ -1,15 +1,13 @@
-# coding: utf-8
 # © 2016 David BEAL @ Akretion <david.beal@akretion.com>
 #        Sébastien Beau <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import fields, models
 
 
 class StockProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
+    _inherit = "stock.production.lot"
 
     config = fields.Serialized(
-        'Configuration',
-        readonly=True,
-        help="Allow to set custom configuration")
+        "Configuration", readonly=True, help="Allow to set custom configuration"
+    )
