b8328cdd4ec09977a5c7612d8a9325a3b37bd2d3
Pierrick Brun <pierrick.brun@akretion.com>
2021-03-04 12:13:36 +0100

    [ADD] database_age_cron module

---
+++ database_age_cron/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Database Age Cron",
+    "summary": "Run a cron that determines database age",
+    "version": "14.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/akretion/ak-odoo-incubator",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/cron.xml",
+    ],
+}
