c838349408f6628212f5f906cd3aa6c676e2105e
Florian da Costa <florian.dacosta@akretion.com>
2021-03-16 14:10:38 +0100

    [MIG] purchase_lot: Migration to 14.0

---
+++ purchase_lot/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Purchase Lot",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/ak-odoo-incubator",
+    "depends": [
+        "purchase",
+        "sale_order_lot_selection",
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+}
---
+++ purchase_lot/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Propagate lot From sale orders to purchase orders.
+Allow to buy specific lot for an sale order (may be usefull for fully configurable products)
