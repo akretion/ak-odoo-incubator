78607ce431e90521e27f2944456a3098b7632637
Sébastien BEAU <sebastien.beau@akretion.com>
2021-06-22 09:13:24 +0200

    [IMP] start module for maintenance and migration pricing

---
+++ module_analysis_price/models/ir_module_author.py
@@ -0,0 +1,47 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class IrModuleAuthor(models.Model):
+    _inherit = "ir.module.author"
+
+    community_installed_module_qty = fields.Integer(
+        compute="_compute_community_rate", store=True
+    )
+    community_installed_module_rate = fields.Float(
+        compute="_compute_community_rate", store=True
+    )
+    community_installed_code_qty = fields.Integer(
+        compute="_compute_community_rate", store=True
+    )
+    community_installed_code_rate = fields.Float(
+        compute="_compute_community_rate", store=True
+    )
+
+    @api.depends(
+        "installed_module_ids.code_qty",
+        "installed_module_ids.module_type_id.community",
+    )
+    def _compute_community_rate(self):
+        type_community = self.env["ir.module.type"].search([("community", "=", True)])
+        if type_community:
+            total_code_qty = sum(type_community.mapped("code_qty"))
+            total_module_qty = len(type_community.installed_module_ids)
+            for record in self:
+                modules = record.installed_module_ids.filtered(
+                    "module_type_id.community"
+                )
+                record.community_installed_code_qty = sum(modules.mapped("code_qty"))
+                record.community_installed_code_rate = (
+                    100 * sum(modules.mapped("code_qty")) / total_code_qty
+                )
+                record.community_installed_module_qty = len(modules)
+                record.community_installed_module_rate = (
+                    100 * len(modules) / total_module_qty
+                )
+        else:
+            for record in self:
+                record.community_rate = 0
---
+++ module_analysis_price/models/ir_module_type.py
@@ -0,0 +1,49 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo import api, fields, models
+
+# number of line per a page of a book
+QTY_PER_PAGE = 35
+
+
+class IrModuleType(models.Model):
+    _inherit = "ir.module.type"
+
+    sequence = fields.Integer()
+    code_qty = fields.Integer(
+        string="Code Quantity", compute="_compute_code_qty", store=True
+    )
+    page_qty = fields.Integer(
+        string="Page Qty", compute="_compute_code_qty", store=True
+    )
+    community = fields.Boolean()
+    migration_price_unit = fields.Float()
+    maintenance_price_unit = fields.Float()
+    migration_monthly_price = fields.Float(compute="_compute_migration_price")
+    migration_year_price = fields.Float(compute="_compute_migration_price")
+    maintenance_monthly_price = fields.Float(compute="_compute_maintenance_price")
+    maintenance_year_price = fields.Float(compute="_compute_maintenance_price")
+
+    @api.depends("migration_price_unit")
+    def _compute_migration_price(self):
+        return self._compute_total_price("migration")
+
+    @api.depends("maintenance_price_unit")
+    def _compute_maintenance_price(self):
+        return self._compute_total_price("maintenance")
+
+    def _compute_total_price(self, case):
+        for record in self:
+            record[f"{case}_monthly_price"] = record[f"{case}_price_unit"] * round(
+                record.code_qty / 100
+            )
+            record[f"{case}_year_price"] = record[f"{case}_monthly_price"] * 12
+
+    @api.depends("installed_module_ids.code_qty")
+    def _compute_code_qty(self):
+        for record in self:
+            record.code_qty = sum(record.installed_module_ids.mapped("code_qty"))
+            record.page_qty = record.code_qty / QTY_PER_PAGE
