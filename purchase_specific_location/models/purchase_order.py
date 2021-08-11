dc27b7d7dfdf3018ead67976ceed1c8e1750800b
Florian da Costa <florian.dacosta@akretion.com>
2021-08-11 20:03:28 +0200

    Rename stock_orderpoint_specific_location to purchase_specific_location and run pre-commit

---
+++ purchase_specific_location/models/purchase_order.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Akretion (<http://www.akretion.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    specific_location_id = fields.Many2one("stock.location", string="Specific Location")
+    default_location_dest_id = fields.Many2one(
+        related="picking_type_id.default_location_dest_id", string="Default Location"
+    )
+
+    def _get_destination_location(self):
+        self.ensure_one()
+        if self.specific_location_id:
+            return self.specific_location_id.id
+        else:
+            return super(PurchaseOrder, self)._get_destination_location()
+
+    @api.onchange("picking_type_id")
+    def onchange_specific_location(self):
+        self.specific_location_id = False
