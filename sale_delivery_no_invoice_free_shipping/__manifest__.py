acc09ae00955021521f7c12b44ef1fbb4cbca77c
Florian da Costa <florian.dacosta@akretion.com>
2021-12-24 14:30:55 +0100

    Add sale_delivery_no_invoice_free_shipping

---
+++ sale_delivery_no_invoice_free_shipping/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Sale Delivery No Invoice Free Shipping",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/ak-odoo-incubator",
+    "category": "Custom",
+    "summary": "Avoid invoicing free Shipping Sale Order line",
+    "depends": ["delivery"],
+    "data": [],
+}
