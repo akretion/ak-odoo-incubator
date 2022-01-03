fbf07edb2291c9dede8ea89c103d29c7031b2c93
Kevin Khao <kevinkhao@gmail.com>
2022-01-03 10:53:39 +0300

    [IMP] Make unique layout reference overloadable

--- mail_unique_layout/models/mail_thread.py
+++ mail_unique_layout/models/mail_thread.py
@@ -7,7 +7,11 @@ from odoo import api, models
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
+    @api.model
+    def _get_unique_layout_ref(self):
+        return "mail_unique_layout.general_mail_layout"
+
     @api.returns("mail.message", lambda value: value.id)
     def message_post(self, *kw, **kwargs):
-        kwargs["email_layout_xmlid"] = "mail_unique_layout.general_mail_layout"
+        kwargs["email_layout_xmlid"] = self._get_unique_layout_ref()
         return super().message_post(*kw, **kwargs)
