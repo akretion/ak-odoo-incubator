4b1d1d5761de16ad3bd578bb1e535337708ee5db
Sébastien BEAU <sebastien.beau@akretion.com>
2022-04-07 01:31:54 +0200

    [IMP] split module in two

---
+++ project_time_in_day/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2022 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project time in days",
+    "summary": "Compute time in days",
+    "version": "12.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "hr_timesheet",
+    ],
+    "data": [
+        "views/project_task_view.xml",
+        "views/project_project_view.xml",
+    ],
+    "demo": [
+    ],
+}
