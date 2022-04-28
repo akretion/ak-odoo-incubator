c365760852b7b47e9569ebc510310c827102d5de
Florian da Costa <florian.dacosta@akretion.com>
2022-04-28 18:02:49 +0200

    Manage multiple transport edi for 1 supplier

--- purchase_edi_file/models/__init__.py
+++ purchase_edi_file/models/__init__.py
@@ -2,3 +2,6 @@ from . import res_partner
 from . import product_supplierinfo
 from . import purchase_order
 from . import purchase_order_line
+from . import edi_transport_config
+from . import partner_export_edi
+from . import ir_exports_config
