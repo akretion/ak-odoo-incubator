8cc10d32ce5a344bc9a3941d3d29c8a165474d8d
Florian da Costa <florian.dacosta@akretion.com>
2022-07-06 11:52:05 +0200

    Linting on project_time_in_day and project_estimate_step

--- project_estimate_step/models/project_estimate_step.py
+++ project_estimate_step/models/project_estimate_step.py
@@ -2,12 +2,12 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class ProjectEstimateStep(models.Model):
-    _name = 'project.estimate.step'
-    _description = 'Project Estimate Step'
+    _name = "project.estimate.step"
+    _description = "Project Estimate Step"
     _order = "days, name"
 
     name = fields.Char(required=True)
--- project_estimate_step/models/project_task_type.py
+++ project_estimate_step/models/project_task_type.py
@@ -2,11 +2,11 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class ProjectTaskType(models.Model):
-    _inherit = 'project.task.type'
+    _inherit = "project.task.type"
 
     to_estimate = fields.Boolean()
     current_sprint = fields.Boolean()
