d8bc35e88c8d8ebb758308147fa70b0416720fca
Florian da Costa <florian.dacosta@akretion.com>
2022-07-06 11:52:05 +0200

    Linting on project_time_in_day and project_estimate_step

--- project_estimate_step/models/project_project.py
+++ project_estimate_step/models/project_project.py
@@ -2,12 +2,12 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     estimate_step_ids = fields.Many2many(
-        comodel_name='project.estimate.step',
-        string='Estimate Step')
+        comodel_name="project.estimate.step", string="Estimate Step"
+    )
