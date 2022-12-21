1a24eb25190a7e17f8dbf4060a35e829672e8b81
Sébastien BEAU <sebastien.beau@akretion.com>
2022-12-21 12:28:35 +0100

    stock_inventory_simple_valuation: refactor code to make it simplier

--- stock_inventory_simple_valuation/models/inventory.py
+++ stock_inventory_simple_valuation/models/inventory.py
@@ -14,10 +14,6 @@ class StockInventory(models.Model):
     _inherit = ["stock.inventory", "mail.thread"]
     _name = "stock.inventory"
 
-    def button_compute_line_costs(self):
-        self.ensure_one()
-        self.line_ids._compute_product_cost()
-
     def action_open_inventory_lines(self):
         action = super().action_open_inventory_lines()
         if self._context.get("valuation"):
@@ -34,41 +30,44 @@ class StockInventoryLine(models.Model):
         compute="_compute_product_cost",
         string="Computed cost",
         store=True,
+        compute_sudo=True,
         digits=dp.get_precision("Account"),
     )
     manual_product_cost = fields.Float(
-        string="Manual cost", digits=dp.get_precision("Account")
+        string="Manual cost",
+        digits=dp.get_precision("Account"),
     )
     total_value = fields.Float(
         compute="_compute_total_value",
         string="Value",
         store=True,
+        compute_sudo=True,
         digits=dp.get_precision("Account"),
     )
     explanation = fields.Text(
         string="Source",
         compute="_compute_product_cost",
         store=True,
+        compute_sudo=True,
         help="Explain computation method for each product",
     )
     origin_record_reference = fields.Char(
-        string="Reference", compute="_compute_product_cost", store=True
+        string="Reference",
+        compute="_compute_product_cost",
+        store=True,
+        compute_sudo=True,
     )
     origin_record_name = fields.Char(
-        string="Document Name", compute="_compute_product_cost", store=True
+        string="Document Name",
+        compute="_compute_product_cost",
+        store=True,
+        compute_sudo=True,
     )
 
-    @api.onchange("manual_product_cost")
-    def _onchange_manual_product_cost(self):
-        self._compute_product_cost()
-
-    @api.depends("calc_product_cost", "manual_product_cost", "product_qty")
+    @api.depends("calc_product_cost", "product_qty")
     def _compute_total_value(self):
         for line in self:
-            if line.manual_product_cost:
-                line.total_value = line.manual_product_cost * line.product_qty
-            else:
-                line.total_value = line.calc_product_cost * line.product_qty
+            line.total_value = line.calc_product_cost * line.product_qty
 
     @api.depends(
         "product_id",
@@ -76,25 +75,21 @@ class StockInventoryLine(models.Model):
         "manual_product_cost",
     )
     def _compute_product_cost(self):
-        # sudo because it is unlikely the user has access rights to all
-        # relevant models
-        self = self.sudo()
-        lines = self.filtered(lambda r: r.inventory_id and r.product_id)
-        lines_manual = lines.filtered(lambda r: r.manual_product_cost)
-        lines_manual._compute_product_cost_manual()
-        lines_calc = lines - lines_manual
         search_methods = self._get_search_methods()
-        for record in lines_calc:
-            for method in search_methods:
-                if getattr(record, method)():
-                    break
-
-    def _compute_product_cost_manual(self):
-        for rec in self:
-            rec.calc_product_cost = rec.manual_product_cost
-            rec.explanation = _("Manual setting")
-            rec.origin_record_reference = _("n/a")
-            rec.origin_record_name = _("n/a")
+        for record in self:
+            if record.inventory_id and record.product_id:
+                for method, method_name in search_methods:
+                    res = getattr(record, method)()
+                    if res:
+                        record.explanation = method_name
+                        record.calc_product_cost, origin = res
+                        record.origin_record_name = origin.display_name
+                        record.origin_record_reference = f"{origin._name},{origin.id}"
+                        break
+                record.explanation = _("No Cost found")
+                record.calc_product_cost = 0.0
+                record.origin_record_reference = None
+                record.origin_record_name = None
 
     def _get_search_methods(self):
         """Overload this to customize price search methods
@@ -102,27 +97,25 @@ class StockInventoryLine(models.Model):
         origin_record_reference, origin_record_name,
         and return a bool indicating a result has been
         found"""
-        res = [
-            "_search_cost_supplierinfo",
-            "_search_cost_invoice_lines",
-            "_search_cost_po_lines",
-            "_search_cost_standard_price",
-            "_search_cost_give_up",
+        return [
+            ("_search_cost_manual", _("Manual setting")),
+            ("_search_cost_invoice_lines", _("Invoice")),
+            ("_search_cost_supplierinfo", _("Supplier info")),
+            ("_search_cost_po_lines", _("Purchase Order")),
+            ("_search_cost_standard_price", _("Standard price")),
         ]
-        return res
+
+    def _search_cost_manual(self):
+        if self.manual_product_cost:
+            return (self.manual_product_cost, None)
 
     def _search_cost_supplierinfo(self):
         sup_info = self.product_id.seller_ids
         if sup_info and sup_info[0].price:
-            self.calc_product_cost = sup_info[0].price
-            self.explanation = _("Supplier info")
-            self.origin_record_reference = "product.supplierinfo,%s" % sup_info[0].id
-            self.origin_record_name = sup_info[0].name.name
-            return True
-        return False
+            return (sup_info[0].price, sup_info[0])
 
     def _search_cost_invoice_lines(self):
-        most_recent_invoice_line = self.env["account.move.line"].search(
+        line = self.env["account.move.line"].search(
             [
                 ("product_id", "=", self.product_id.id),
                 ("move_id.state", "=", "posted"),
@@ -131,15 +124,8 @@ class StockInventoryLine(models.Model):
             order="create_date desc",
             limit=1,
         )
-        if most_recent_invoice_line:
-            self.calc_product_cost = most_recent_invoice_line.price_unit
-            self.explanation = _("Invoice")
-            self.origin_record_reference = (
-                "account.invoice,%s" % most_recent_invoice_line.move_id.id
-            )
-            self.origin_record_name = most_recent_invoice_line.move_id.name
-            return True
-        return False
+        if line:
+            return (line.price_unit, line.move_id)
 
     def _search_cost_po_lines(self):
         po_line = self.env["purchase.order.line"].search(
@@ -150,25 +136,8 @@ class StockInventoryLine(models.Model):
             limit=1,
         )
         if po_line:
-            self.calc_product_cost = po_line.price_unit
-            self.explanation = _("Purchase Order")
-            self.origin_record_reference = "purchase.order,%s" % po_line.order_id.id
-            self.origin_record_name = po_line.order_id.name
-            return True
-        return False
+            return (po_line.price_unit, po_line.order_id)
 
     def _search_cost_standard_price(self):
         if self.product_id.standard_price:
-            self.calc_product_cost = self.product_id.standard_price
-            self.explanation = _("Standard price")
-            self.origin_record_reference = "product.product,%s" % self.product_id.id
-            self.origin_record_name = self.product_id.name
-            return True
-        return False
-
-    def _search_cost_give_up(self):
-        self.explanation = _("No Cost found")
-        self.calc_product_cost = 0.0
-        self.origin_record_reference = "n/a"
-        self.origin_record_name = _("None")
-        return True
+            return (self.product_id.standard_price, self.product_id)
