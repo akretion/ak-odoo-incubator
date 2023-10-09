df583e768b77bb8ff598bfc63592d2bd8b213962
David Beal <david.beal@akretion.com>
2023-10-09 23:54:14 +0200

    ADD base_tool module

---
+++ base_tool/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Base Tools",
+    "summary": "Utilities that can be used in projects",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/akretion/ak-odoo-incubator",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [],
+    "installable": True,
+}
