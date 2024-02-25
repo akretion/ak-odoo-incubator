94c91886443bf879cda4e05709a3d8b78eb18907
Sébastien BEAU <sebastien.beau@akretion.com>
2024-02-25 11:58:55 +0100

    mail_unique_layout: refactor code so we can use specific layout for some record

--- mail_unique_layout/wizards/mail_compose_message.py
+++ mail_unique_layout/wizards/mail_compose_message.py
@@ -9,7 +9,6 @@ class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
     def send_mail(self, auto_commit=False):
-        self = self.with_context(
-            custom_layout=self.env["mail.thread"]._get_unique_layout_ref()
-        )
+        record = self.env[self.model].browse(self.res_id)
+        self = self.with_context(custom_layout=record._get_mail_layout())
         return super().send_mail(auto_commit=auto_commit)
