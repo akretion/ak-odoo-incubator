404cf71e2fd319e581494ef1fa23c4032c21c9f9
Mourad <mourad.elhadj.mimoune@akretion.com>
2024-03-20 11:51:25 +0100

    [16.0][ADD] export_invoice_edi_auchan module

---
+++ export_invoice_edi_auchan/schema/base.py
@@ -0,0 +1,58 @@
+# Copyright 2023 Akretion
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import datetime
+
+import freezegun
+from unidecode import unidecode
+
+from odoo.tools import float_compare
+
+
+class SegmentInterface:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def _format_values(self, size, value="", ctx=False):
+        if not ctx:
+            ctx = {}
+        if not value:
+            value = ""
+        if isinstance(value, freezegun.api.FakeDate):
+            fmt_val = datetime.date.strftime(value, "%d/%m/%Y")
+        elif isinstance(value, datetime.datetime):
+            fmt_val = datetime.date.strftime(value.date(), "%d/%m/%Y %H:%M")
+        elif isinstance(value, int):
+            fmt_val = str(value)
+        elif isinstance(value, float):
+            if float_compare(value, 0, 3) == 0 and ctx.get("empty_if_zero"):
+                fmt_val = ""
+            else:
+                if ctx.get("decimal_3"):
+                    fmt_val = str("{:.3f}".format(value))
+                else:
+                    fmt_val = str("{:.2f}".format(value))
+        elif isinstance(value, str):
+            fmt_val = unidecode(value)
+        else:
+            raise ValueError(f"Unsupported value type: {type(value)}")
+        if len(fmt_val) > size:
+            if ctx.get("truncate_silent"):
+                fmt_val = fmt_val[:size]
+            else:
+                errorstr = "{} trop long, taille maximale est de {}".format(
+                    fmt_val, size
+                )
+                if ctx:
+                    errorstr = "contexte: {}".format(ctx) + errorstr
+                raise ValueError(errorstr)
+        return fmt_val
+
+    def render(self):
+        res = ""
+        for fmt_data in self.get_values():
+            fmt_val = self._format_values(*fmt_data)
+            res += fmt_val + ";"
+        return res[:-1]
+
+    def get_values(self):
+        raise NotImplementedError
