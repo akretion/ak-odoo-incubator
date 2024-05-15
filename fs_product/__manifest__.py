0ad6ac6958392d27f37aeb44f382aeb32dfc0178
David Beal <david.beal@akretion.com>
2024-05-15 16:13:50 +0200

    ADD fs_product module

---
+++ fs_product/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "File Storage Product",
+    "version": "16.0.1.0.0",
+    "author": "Akretion",
+    "category": "Product",
+    "depends": [
+        "fs_product_multi_image",  # OCA/storage
+    ],
+    "website": "https://github.com/akretion/ak-odoo-incubator",
+    "data": [
+        "views/product_template.xml",
+    ],
+    "maintainer": ["bealdav"],
+    "license": "AGPL-3",
+    "installable": True,
+}
---
+++ fs_product/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Usability additional module for File Storage module
+
+    - filters on products
