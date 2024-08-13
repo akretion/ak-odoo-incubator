d165af8436e7828d2d0976619f93c09705beaa19
Mathieu <mathieu.delva@hotmail.com>
2024-08-13 14:39:37 +0200

    [FIX] bug zip product image

--- zip_product_image/models/product.py
+++ zip_product_image/models/product.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Akretion
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import base64
 from odoo import api, models
 
 LIMIT = 500
@@ -26,6 +27,6 @@ class ProductProduct(models.Model):
         if "fs_product_multi_image" in self.env.registry._init_modules:
             # Here fs_product_multi_image is installed
             if self.image:
-                return (self.image.getvalue(), self.image.extension)
+                return (base64.b64encode(self.image.getvalue()), self.image.extension)
             return (False, False)
         return (self.image_1920 or False, '.jpg')
