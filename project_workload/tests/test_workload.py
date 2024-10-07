5b5623e2f205933d67a50518018c04390687ed42
Florian Mounier <paradoxxx.zero@gmail.com>
2024-10-07 17:20:56 +0200

    [IMP] project_workload: Make tests coherent with additions tests

--- project_workload/tests/test_workload.py
+++ project_workload/tests/test_workload.py
@@ -11,7 +11,7 @@ from odoo.addons.project_workload.tests.common import TestWorkloadCommon
 class TestWorkload(TestWorkloadCommon):
     def _create_task(self):
         now = datetime.now()
-        self.task = self.env["project.task"].create(
+        return self.env["project.task"].create(
             {
                 "name": "Task 1",
                 "project_id": self.project.id,
@@ -23,11 +23,11 @@ class TestWorkload(TestWorkloadCommon):
         )
 
     def test_task_assign_with_hours(self):
-        self._create_task()
-        workload = self.task.workload_ids
+        task = self._create_task()
+        workload = task.workload_ids
         self.assertEqual(len(workload), 1)
-        self.assertEqual(workload.date_start, self.task.planned_date_start.date())
-        self.assertEqual(workload.date_end, self.task.planned_date_end.date())
+        self.assertEqual(workload.date_start, task.planned_date_start.date())
+        self.assertEqual(workload.date_end, task.planned_date_end.date())
         self.assertEqual(workload.hours, 21)
         load_unit = workload.unit_ids
         self.assertEqual(len(load_unit), 3)
@@ -35,34 +35,32 @@ class TestWorkload(TestWorkloadCommon):
         self.assertEqual(set(load_unit.mapped("hours")), {7})
 
     def test_change_user(self):
-        self._create_task()
-        self.task.user_ids = [(6, 0, [self.user_2.id])]
-        self.assertEqual(self.task.workload_ids.user_id, self.user_2)
-        self.assertEqual(self.task.workload_ids.unit_ids.user_id, self.user_2)
+        task = self._create_task()
+        task.user_ids = [(6, 0, [self.user_2.id])]
+        self.assertEqual(task.workload_ids.user_id, self.user_2)
+        self.assertEqual(task.workload_ids.unit_ids.user_id, self.user_2)
 
     def test_add_user(self):
-        self._create_task()
-        self.task.user_ids = [(4, self.user_2.id)]
-        self.assertEqual(len(self.task.workload_ids), 2)
+        task = self._create_task()
+        task.user_ids = [(4, self.user_2.id)]
+        self.assertEqual(len(task.workload_ids), 2)
+        self.assertEqual(task.workload_ids.mapped("user_id"), self.user_1 + self.user_2)
         self.assertEqual(
-            self.task.workload_ids.mapped("user_id"), self.user_1 + self.user_2
-        )
-        self.assertEqual(
-            self.task.workload_ids.unit_ids.mapped("user_id"), self.user_1 + self.user_2
+            task.workload_ids.unit_ids.mapped("user_id"), self.user_1 + self.user_2
         )
 
     def test_remove_user(self):
-        self._create_task()
-        self.task.user_ids = [(5, 0, 0)]
-        self.assertFalse(self.task.workload_ids)
+        task = self._create_task()
+        task.user_ids = [(5, 0, 0)]
+        self.assertFalse(task.workload_ids)
 
     def test_change_date(self):
-        self._create_task()
-        self.task.planned_date_end = self.task.planned_date_start + timedelta(days=13)
-        workload = self.task.workload_ids
+        task = self._create_task()
+        task.planned_date_end = task.planned_date_start + timedelta(days=13)
+        workload = task.workload_ids
         self.assertEqual(len(workload), 1)
-        self.assertEqual(workload.date_start, self.task.planned_date_start.date())
-        self.assertEqual(workload.date_end, self.task.planned_date_end.date())
+        self.assertEqual(workload.date_start, task.planned_date_start.date())
+        self.assertEqual(workload.date_end, task.planned_date_end.date())
         self.assertEqual(workload.hours, 21)
         load_unit = workload.unit_ids
         self.assertEqual(len(load_unit), 2)
