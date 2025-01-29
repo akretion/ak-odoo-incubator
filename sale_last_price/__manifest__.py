a3e137e6cbffe5aa8aa1363c9b0f05e90fa0839c
Florian da Costa <florian.dacosta@akretion.com>
2025-01-29 12:24:27 +0100

    Bump version

--- sale_last_price/__manifest__.py
+++ sale_last_price/__manifest__.py
@@ -6,7 +6,7 @@
         Display on sale order lines the last sale price coming from an old system
         for control during a migration
     """,
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/akretion/ak-odoo-incubator",
