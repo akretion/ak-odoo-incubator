a294172f173a1ac143daae247f3d1e63b5879e81
Florian Mounier <paradoxxx.zero@gmail.com>
2025-08-21 19:01:21 +0200

    [IMP] sixteen_in_fourteen: Backport of Command and some sale consts

---
+++ sixteen_in_fourteen/odoo/addons/sale/models/sale_order.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+READONLY_FIELD_STATES = {
+    state: [("readonly", True)] for state in {"sale", "done", "cancel"}
+}
+
+LOCKED_FIELD_STATES = {state: [("readonly", True)] for state in {"done", "cancel"}}
+
+INVOICE_STATUS = [
+    ("upselling", "Upselling Opportunity"),
+    ("invoiced", "Fully Invoiced"),
+    ("to invoice", "To Invoice"),
+    ("no", "Nothing to Invoice"),
+]
