c68c554d02eecf0595ce8f752ba30da55db7235a
Florian Mounier <paradoxxx.zero@gmail.com>
2025-09-18 11:19:07 +0200

    [IMP] sixteen_in_fourteen: Backport _xmlid_to_res_id (xmlid_to_res_id rename)

---
+++ sixteen_in_fourteen/odoo/addons/base/models/ir_model.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.base.models.ir_model import IrModelData
+
+IrModelData._xmlid_to_res_id = IrModelData.xmlid_to_res_id
