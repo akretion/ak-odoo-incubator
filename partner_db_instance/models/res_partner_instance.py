00b0cb7282a7eea52f34a8dfd472a7afbff31aae
Florian Mounier <paradoxxx.zero@gmail.com>
2026-09-01 15:17:08 +0200

    [IMP] partner_db_instance: Improve state management and database sync

--- partner_db_instance/models/res_partner_instance.py
+++ partner_db_instance/models/res_partner_instance.py
@@ -30,7 +30,10 @@ db_drop = exp_drop.__wrapped__
 
 class ResPartnerInstance(models.Model):
     _name = "res.partner.instance"
+    _description = "Partner Database Instance"
+    _inherit = ("mail.thread", "mail.activity.mixin")
     _rec_name = "subdomain"
+    _order = "state desc, partner_id"
 
     state = fields.Selection(
         [
@@ -39,6 +42,10 @@ class ResPartnerInstance(models.Model):
             ("archived", "Archived"),
         ],
         compute="_compute_state",
+        store=True,
+        tracking=True,
+        default="draft",
+        required=True,
     )
     partner_id = fields.Many2one("res.partner", required=True)
     subdomain = fields.Char(
@@ -47,20 +54,18 @@ class ResPartnerInstance(models.Model):
         precompute=True,
         required=True,
         readonly=False,
-    )
-    db_name = fields.Char(
-        compute="_compute_db_name",
-        store=True,
-        readonly=True,
+        tracking=True,
     )
     url = fields.Char(
         compute="_compute_url",
         readonly=True,
     )
 
-    _sql_constraints = [
+    _sql_constraints = (
         ("subdomain_unique", "UNIQUE(subdomain)", "Subdomain must be unique"),
-    ]
+        ("partner_unique", "UNIQUE(partner_id)", "Partner must be unique"),
+        ("db_name_unique", "UNIQUE(db_name)", "DB name must be unique"),
+    )
 
     @api.constrains("subdomain")
     def _check_subdomain(self):
@@ -87,8 +92,26 @@ class ResPartnerInstance(models.Model):
         return instance_admin_password
 
     @property
+    def db_name(self):
+        base = self.master_instance.split("_")[0]
+        return f"{base}_{self.subdomain}"
+
+    @property
     def archived_db_name(self):
         return f"_archived_{self.db_name}"
+
+    @contextmanager
+    def instance_env(self, update_module=False):
+        registry = Registry.new(self.db_name, False, None, update_module=update_module)
+        with closing(registry.cursor()) as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            try:
+                yield env
+            except Exception:
+                cr.rollback()
+                raise
+            else:
+                cr.commit()  # pylint: disable=invalid-commit
 
     @api.depends("subdomain")
     def _compute_url(self):
@@ -130,66 +153,21 @@ class ResPartnerInstance(models.Model):
             else:
                 record.state = "draft"
 
-    @api.depends("partner_id")
-    def _compute_db_name(self):
-        for record in self:
-            base = self.master_instance.split("_")[0]
-            if not record.db_name or record.state == "draft":
-                record.db_name = f"{base}_{record._clean_partner_name()}"
-
-    def action_deploy(self):
-        for record in self:
-            record._setup_database()
-
-    def action_archive(self):
-        for record in self:
-            close_db(record.db_name)
-            db_rename(record.db_name, record.archived_db_name)
-
-    def action_unarchive(self):
-        for record in self:
-            db_rename(record.archived_db_name, record.db_name)
-
-    def action_delete(self):
-        for record in self:
-            db_drop(record.archived_db_name)
-
-    @contextmanager
-    def instance_env(self):
-        registry = Registry.new(self.db_name, False, None, update_module=True)
-        with closing(registry.cursor()) as cr:
-            env = api.Environment(cr, SUPERUSER_ID, {})
-            try:
-                yield env
-            except Exception:
-                cr.rollback()
-                raise
-            else:
-                cr.commit()  # pylint: disable=invalid-commit
-
-    def action_connect(self):
-        self.ensure_one()
-        master_user = self.env.user
-
-        if not master_user.has_group(
-            "partner_db_instance.group_partner_instance_manager"
-        ):
-            raise AccessDenied()
+    def _install_modules(self):
+        modules_to_install = (
+            config.get("partner_db_instance_modules") or "db_instance"
+        ).split(",")
 
         with self.instance_env() as env:
-            user = env["res.users"].search([("login", "=", master_user.login)])
-            if not user:
-                user = env["res.users"].create(
-                    {"login": master_user.login, "name": master_user.name}
+            modules = env["ir.module.module"].search(
+                [("name", "in", modules_to_install)]
+            )
+            if len(modules) < len(modules_to_install):
+                _logger.warning(
+                    "Not all instance modules were installed: "
+                    f"{modules_to_install} -> {modules.mapped('name')}"
                 )
-            token = user._get_instance_db_jwt_token()
-            user_id = user.id
-
-        return {
-            "type": "ir.actions.act_url",
-            "url": f"{self.url}/web/instance/{user_id}/{token}/connect",
-            "target": "new",
-        }
+            modules.button_immediate_install()
 
     def _setup_database(self):
         self.ensure_one()
@@ -218,17 +196,102 @@ class ResPartnerInstance(models.Model):
             self.env.company.country_id.code,
             self.env.company.phone,
         )
-        modules_to_install = (
-            config.get("partner_db_instance_modules") or "db_instance"
-        ).split(",")
+
+        self._install_modules()
+
+    def action_deploy(self):
+        for record in self:
+            record.message_post(
+                body=self.env._("Deploying database %s", record.db_name)
+            )
+            record._setup_database()
+        self._compute_state()
+
+    def action_archive(self):
+        for record in self:
+            record.message_post(
+                body=self.env._("Archiving database %s", record.db_name)
+            )
+            close_db(record.db_name)
+            db_rename(record.db_name, record.archived_db_name)
+        self._compute_state()
+
+    def action_unarchive(self):
+        for record in self:
+            record.message_post(
+                body=self.env._("Redeploying database %s", record.db_name)
+            )
+            db_rename(record.archived_db_name, record.db_name)
+        self._compute_state()
+
+    def action_delete(self):
+        for record in self:
+            record.message_post(body=self.env._("Deleted database %s", record.db_name))
+            db_drop(record.archived_db_name)
+        self._compute_state()
+
+    def action_connect(self):
+        self.ensure_one()
+        master_user = self.env.user
+
+        if not master_user.has_group(
+            "partner_db_instance.group_partner_instance_manager"
+        ):
+            raise AccessDenied()
 
         with self.instance_env() as env:
-            modules = env["ir.module.module"].search(
-                [("name", "in", modules_to_install)]
-            )
-            if len(modules) < len(modules_to_install):
-                _logger.warning(
-                    "Not all instance modules were installed: "
-                    f"{modules_to_install} -> {modules.mapped('name')}"
+            user = env["res.users"].search([("login", "=", master_user.login)])
+            if not user:
+                user = env["res.users"].create(
+                    {"login": master_user.login, "name": master_user.name}
                 )
-            modules.button_immediate_install()
+            token = user._get_instance_db_jwt_token()
+            user_id = user.id
+
+        return {
+            "type": "ir.actions.act_url",
+            "url": f"{self.url}/web/instance/{user_id}/{token}/connect",
+            "target": "new",
+        }
+
+    def action_sync_state(self):
+        self._compute_state()
+
+    def action_sync_state_all(self):
+        self.search([])._compute_state()
+
+    def write(self, vals):
+        state = self.state
+        old_db_name = (
+            (self.archived_db_name if state == "archived" else self.db_name)
+            if state != "draft"
+            else None
+        )
+        result = super().write(vals)
+        new_db_name = self.archived_db_name if state == "archived" else self.db_name
+        if old_db_name and new_db_name != old_db_name:
+            self.message_post(
+                body=self.env._(
+                    "Renamed database from %s to %s", old_db_name, new_db_name
+                )
+            )
+            close_db(old_db_name)
+            db_rename(old_db_name, new_db_name)
+            self._compute_state()
+
+        return result
+
+    def unlink(self):
+        self._compute_state()
+        db_name = (
+            (self.archived_db_name if self.state == "archived" else self.db_name)
+            if self.state != "draft"
+            else None
+        )
+
+        res = super().unlink()
+        # Close and drop the database if it exists
+        if db_name:
+            close_db(db_name)
+            db_drop(db_name)
+        return res
